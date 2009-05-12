0875a74ea370023e3228b477d8408d861a09d1a8
krzysz.zienki <krzysz.zienki@gmail.com>
2009-05-12 16:21:20 +0000

    Correctly adds pins to a map

--- src/edu/vanderbilt/isis/vuphone/Map.java
+++ src/edu/vanderbilt/isis/vuphone/Map.java
@@ -1,20 +1,34 @@
 package edu.vanderbilt.isis.vuphone;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
+import android.widget.TextView;
+
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
 import com.google.android.maps.OverlayItem;
+import com.google.android.maps.Projection;
+
+class DebugTable{
+	public TextView mode, screenPos, geoPos, pins;
+}
 
 public class Map extends MapActivity {
 
 	private final int MENU_ADD_POINT = 0;
 	private final int MENU_QUIT = 1;
 	
+	private int MODE_NAVIGATE = 0;
+	private int MODE_PIN = 1;
+	
+	private int MODE = MODE_NAVIGATE;
+	
 	private MapView mapView;
+	private DebugTable debug;
 	
 	/** Called when the activity is first created. */
     @Override
@@ -23,15 +37,49 @@ public class Map extends MapActivity {
         setContentView(R.layout.main);
         
         mapView = (MapView) findViewById(R.id.mapview);
-        mapView = new MapView(this,"0zz3AmUYDBIjlLBRuJlLd74N-jLfx2mDwJG4cDQ");
+        
         mapView.setBuiltInZoomControls(true);
         
-        GeoPoint point = new GeoPoint(19240000,-99120000);
-        addPoint(point);
+        debug = new DebugTable();
+        
+        debug.mode = (TextView) findViewById(R.id.mode);
+        debug.screenPos = (TextView) findViewById(R.id.screen_pos);
+        debug.geoPos = (TextView) findViewById(R.id.geo_pos);
+        debug.pins = (TextView) findViewById(R.id.pins);
+        
+        debug.mode.setText("Mode: " + MODE);
+        
+        debug.mode.setText("" + mapView.getTop());
     }
     
-    public boolean onTouchEvent(MotionEvent event){
-    	return super.onTouchEvent(event);
+    // THE CALCULATED GEO POINTS ARE ALWAYS OFF BY THE SAME AMOUNT.
+    // SCREEN POS NEEDS TO BE ADJUSTED TO MAPVIEW
+    public boolean dispatchTouchEvent(MotionEvent event){
+    
+    	float x = event.getX();// - mapView.getLeft();
+    	float y = event.getY();// - mapView.getTop();
+    	
+    	int offset[] = new int[2];
+    	mapView.getLocationOnScreen(offset);
+    	x -= (float) offset[0];
+    	y -= (float) offset[1];
+    	    	
+    	Projection proj = mapView.getProjection();
+    	GeoPoint pt = proj.fromPixels((int) x, (int) y);
+    	
+    	String scr = "Screen: " + x + ", " + y;
+    	String geo = "Geo: " + pt.toString();
+    	
+    	debug.screenPos.setText(scr);
+    	debug.geoPos.setText(geo);
+    	
+    	if (MODE == MODE_PIN){
+    		addPoint(pt);
+    		MODE = 10;
+    		return true;
+    	}else{    	
+    		return super.dispatchTouchEvent(event);
+    	}
     }
     
     public void addPoint(GeoPoint point){
@@ -40,6 +88,10 @@ public class Map extends MapActivity {
 		overlay.addOverlay(item);
 		
 		mapView.getOverlays().add(overlay);
+		
+		String pinsNum = "" + mapView.getOverlays().size();
+		debug.pins.setText(pinsNum);
+		mapView.postInvalidate();
     }
     
     protected boolean isRouteDisplayed() {
@@ -56,6 +108,8 @@ public class Map extends MapActivity {
     public boolean onOptionsItemSelected(MenuItem item){
     	switch(item.getItemId()){
     	case MENU_ADD_POINT:
+    		MODE = MODE_PIN;
+    		debug.mode.setText("Mode: " + MODE);
     		return true;
     	case MENU_QUIT:
     		return true;
