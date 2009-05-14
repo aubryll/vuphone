28e1eba43ad3d4f0940c564ce3d2b5693d23cc9f
hamiltont <hamiltont@963311a8-3e45-11de-917f-4137a9cc17b9>
2009-05-14 20:19:23 +0000

    ignored the bin directory

--- src/edu/vanderbilt/isis/vuphone/tools/PrecisionPoint.java
+++ src/edu/vanderbilt/isis/vuphone/tools/PrecisionPoint.java
@@ -6,7 +6,15 @@ public class PrecisionPoint extends Point {
 	public float x;
 	public float y;
 	
+	public PrecisionPoint() {
+	}
+	
 	public PrecisionPoint(Point p) {
+		this.set(p);
+	}
+	
+	public void set(Point p)
+	{
 		x = p.x;
 		y = p.y;
 	}
