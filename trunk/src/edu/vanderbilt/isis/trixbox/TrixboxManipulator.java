b180a6f311dff6880bf8ce658b48c2602f674f9f
shcampbell7 <shcampbell7@963311a8-3e45-11de-917f-4137a9cc17b9>
2009-05-15 15:13:50 +0000

    Changed TrixboxManipulator to be a normal non-Activity based class

--- trunk/src/edu/vanderbilt/isis/trixbox/TrixboxManipulator.java
+++ trunk/src/edu/vanderbilt/isis/trixbox/TrixboxManipulator.java
@@ -4,25 +4,15 @@ package edu.vanderbilt.isis.trixbox;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
-//import org.apache.http.HttpException;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
-//import org.apache.http.entity.BasicHttpEntity;
 import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.widget.TextView;
+public class TrixboxManipulator {
 
-public class TrixboxManipulator extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-    	super.onCreate(savedInstanceState);
-        TextView tv = new TextView(this);
-
+    public static void doPost(String newExtension) {
 
         try{
         		// Do the first POST to set the FollowMe List
@@ -32,14 +22,13 @@ public class TrixboxManipulator extends Activity {
                 post.addHeader("Authorization","Basic "+authorization);
                 post.addHeader("Content-Type","application/x-www-form-urlencoded");
                 
-                String info = "display=findmefollow&action=edtGRP&account=201&pre_ring=0&strategy=ringallv2&grptime=20&grplist=203&annmsg=&ringing=Ring&grppre=&dring=&remotealert=&toolate=&Terminate_Call0=app-blackhole%2Changup%2C1&Extensions0=from-did-direct%2C200%2C1&";
+                String info = "display=findmefollow&action=edtGRP&account=201&pre_ring=0&strategy=ringallv2&grptime=20&grplist="+newExtension+"&annmsg=&ringing=Ring&grppre=&dring=&remotealert=&toolate=&Terminate_Call0=app-blackhole%2Changup%2C1&Extensions0=from-did-direct%2C200%2C1&";
                 ByteArrayEntity str = new ByteArrayEntity(info.getBytes());
                 post.setEntity(str);
                 HttpResponse resp = c.execute(post);
                 ByteArrayOutputStream bao = new ByteArrayOutputStream();
                 resp.getEntity().writeTo(bao);
                 String responsetext = new String(bao.toByteArray());
-                tv.setText(responsetext);
                 
                 
                 // Do the final POST to finalize the changes.
@@ -52,18 +41,15 @@ public class TrixboxManipulator extends Activity {
                 HttpResponse resp2 = c.execute(post2);
                 ByteArrayOutputStream bao2 = new ByteArrayOutputStream();
                 resp2.getEntity().writeTo(bao2);
-                String responsetext2 = new String(bao2.toByteArray());
-                tv.setText("Post 2 Response"+responsetext2);        
+                String responsetext2 = new String(bao2.toByteArray());     
         } 
         catch (IOException ioe) {
-                tv.setText("IOException error:"+ioe.getMessage());
+                System.out.println("IOException error:"+ioe.getMessage());
         }
         catch (Exception e) {
-        	 	tv.setText("Other Exception of type:"+e.getClass());
-        	 	tv.append("The message is: "+e.getMessage());
+        	 	System.out.println("Other Exception of type:"+e.getClass());
+        	 	System.out.println("The message is: "+e.getMessage());
         }
-
-        setContentView(tv);
 
     }
 }
