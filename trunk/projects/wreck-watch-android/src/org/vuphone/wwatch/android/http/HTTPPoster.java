2d2d8cc1aff88fac7a471236c60fe074b98a67dc
thompchr <thompchr@963311a8-3e45-11de-917f-4137a9cc17b9>
2009-05-28 18:37:29 +0000

    Finally got the HTTPPoster and server in sync to work together.  Working currently for accident type notifications.

--- trunk/projects/wreck-watch-android/src/org/vuphone/wwatch/android/http/HTTPPoster.java
+++ trunk/projects/wreck-watch-android/src/org/vuphone/wwatch/android/http/HTTPPoster.java
@@ -34,14 +34,16 @@ public class HTTPPoster {
 
 
 	//Chris's test server\\
-	private static final String SERVER = "http://dorm.cmthompson.net";
-	private static final String PATH = "/wreckwatch/test.php";
+//	private static final String SERVER = "http://dorm.cmthompson.net";
+//	private static final String PATH = "/wreckwatch/test.php";
 
 	//Jules's Jetty server\\
 	//Note this is equiv to localhost although the phone has to have an
 	//IP because it's not running it! :)
-	//private static final String SERVER = "http://129.59.129.151:8080";
-	//private static final String PATH = "/wreckwatch/notifications";
+	private static final String SERVER = "http://129.59.129.151:8080";
+	private static final String PATH = "/wreckwatch/notifications";
+	
+	private static final String LOG_LABEL = "VUPHONE";
 
 	//There might be a better way to do these methods....
 	/**
@@ -49,11 +51,15 @@ public class HTTPPoster {
 	 * WreckWatch server.
 	 * @param message
 	 */
-	public String doAccidentPost(double time, double speed, double dec, ArrayList<Waypoint> route){
-
+	public String doAccidentPost(Long time, Double speed, Double dec, ArrayList<Waypoint> route){
 
 		try{
-
+			String timeStr = URLEncoder.encode(time.toString(), "UTF-8");
+			String speedStr = URLEncoder.encode(speed.toString(), "UTF-8");
+			String decStr = URLEncoder.encode(dec.toString(), "UTF-8");
+			
+			
+			Log.v(LOG_LABEL, "Entering HTTPPoster.doPost");
 			HttpClient c = new DefaultHttpClient();
 			HttpPost post = new HttpPost(SERVER + PATH);
 			post.addHeader("Content-Type","application/x-www-form-urlencoded");
@@ -62,31 +68,37 @@ public class HTTPPoster {
 			
 			//Create the parameter string
 			if (route != null){
-				params = "time="+time+"&speed="+speed+"&dec="+dec+"&numpoints="+route.size();
+				params = "type=accident&time="+timeStr+"&speed="+speedStr+"&dec="+decStr+"&numpoints="+route.size();
 
 				for (int i = 0; i < route.size(); ++i){
 					params = params + "&point"+i+"="+URLEncoder.encode(route.get(i).toString(), "UTF-8");
 				}
 			}else{
-				params = "time="+time+"&speed="+speed+"&dec="+dec;
+				params = "type=accident&time="+ timeStr +"&speed="+speedStr+"&dec="+decStr+"&numpoints=0";
 			}
+			
 
 			//Add the parameters
+			Log.v(LOG_LABEL, "Created parameter string: " + params);
 			post.setEntity(new ByteArrayEntity(params.getBytes()));
 
 			//Do it
+			Log.i(LOG_LABEL, "Executing post to " + SERVER + PATH);
 			HttpResponse resp = c.execute(post);
 
 			//Return the HTTP response back to the user in the form of 
 			//a String
 			ByteArrayOutputStream bao = new ByteArrayOutputStream();
 			resp.getEntity().writeTo(bao);
-			return new String(bao.toByteArray());
+			String s = new String(bao.toByteArray());
+			Log.d(LOG_LABEL, "Response from server: " + s);
+			Log.v(LOG_LABEL, "Leaving HTTPPoster.doPost");
+			return s;
 
 		} 
 		catch (Exception e) {
-			Log.v("VUPHONE", "Other Exception of type:"+e.getClass());
-			Log.v("VUPHONE", "The message is: "+e.getMessage());
+			Log.e("VUPHONE", "Other Exception of type:"+e.getClass());
+			Log.e("VUPHONE", "The message is: "+e.getMessage());
 			return null;
 		}
 
