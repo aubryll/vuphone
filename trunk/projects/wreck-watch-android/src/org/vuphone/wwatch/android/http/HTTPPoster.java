227120f54e79f3d62168ce16b4baf62b5251252c
thompchr <thompchr@963311a8-3e45-11de-917f-4137a9cc17b9>
2009-05-29 16:09:10 +0000

    Fixed bug that would cause app to hang if no server was available.  Fixed the way the test lines in ServiceUI called HTTPPoster to correspond to static nature of methods

--- trunk/projects/wreck-watch-android/src/org/vuphone/wwatch/android/http/HTTPPoster.java
+++ trunk/projects/wreck-watch-android/src/org/vuphone/wwatch/android/http/HTTPPoster.java
@@ -16,10 +16,12 @@
 package org.vuphone.wwatch.android.http;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.net.URLEncoder;
 import java.util.List;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ByteArrayEntity;
@@ -34,16 +36,17 @@ public class HTTPPoster {
 
 
 	//Chris's test server\\
-//	private static final String SERVER = "http://dorm.cmthompson.net";
-//	private static final String PATH = "/wreckwatch/test.php";
+	//	private static final String SERVER = "http://dorm.cmthompson.net";
+	//	private static final String PATH = "/wreckwatch/test.php";
 
 	//Jules's Jetty server\\
 	//Note this is equiv to localhost although the phone has to have an
 	//IP because it's not running it! :)
 	private static final String SERVER = "http://129.59.129.151:8080";
 	private static final String PATH = "/wreckwatch/notifications";
-	
+
 	private static final String LOG_LABEL = "VUPHONE";
+	private static final String LOG_MSG_PREFIX = "HTTPPoster: ";
 
 	//There might be a better way to do these methods....
 	/**
@@ -51,57 +54,74 @@ public class HTTPPoster {
 	 * WreckWatch server.
 	 * @param message
 	 */
-	public static String doAccidentPost(Long time, Double speed, Double dec, List<Waypoint> route){
+	public static void doAccidentPost(Long time, Double speed, Double dec, List<Waypoint> route){
 
 		try{
 			String timeStr = URLEncoder.encode(time.toString(), "UTF-8");
 			String speedStr = URLEncoder.encode(speed.toString(), "UTF-8");
 			String decStr = URLEncoder.encode(dec.toString(), "UTF-8");
-			
-			
-			Log.v(LOG_LABEL, "Entering HTTPPoster.doAccidentPost");
-			HttpClient c = new DefaultHttpClient();
-			HttpPost post = new HttpPost(SERVER + PATH);
+
+
+			Log.v(LOG_LABEL, LOG_MSG_PREFIX + "Entering HTTPPoster.doAccidentPost");
+			final HttpClient c = new DefaultHttpClient();
+			final HttpPost post = new HttpPost(SERVER + PATH);
 			post.addHeader("Content-Type","application/x-www-form-urlencoded");
 
-			String params;
-			
+			StringBuffer params = new StringBuffer();
+
 			//Create the parameter string
 			if (route != null){
-				params = "type=accident&time="+timeStr+"&speed="+speedStr+"&dec="+decStr+"&numpoints="+route.size();
+				params.append("type=accident&time="+timeStr+"&speed="+speedStr+"&dec="+decStr+"&numpoints="+route.size());
 
 				for (int i = 0; i < route.size(); ++i){
-					params = params + "&point"+i+"="+URLEncoder.encode(route.get(i).toString(), "UTF-8");
+					params.append("&point"+i+"="+URLEncoder.encode(route.get(i).toString(), "UTF-8"));
 				}
 			}else{
-				params = "type=accident&time="+ timeStr +"&speed="+speedStr+"&dec="+decStr+"&numpoints=0";
+				params.append("type=accident&time="+ timeStr +"&speed="+speedStr+"&dec="+decStr+"&numpoints=0");
 			}
-			
+
+
 
 			//Add the parameters
-			Log.v(LOG_LABEL, "Created parameter string: " + params);
-			post.setEntity(new ByteArrayEntity(params.getBytes()));
+			Log.v(LOG_LABEL, LOG_MSG_PREFIX + "Created parameter string: " + params);
+			post.setEntity(new ByteArrayEntity(params.toString().getBytes()));
+
 
 			//Do it
-			Log.i(LOG_LABEL, "Executing post to " + SERVER + PATH);
-			HttpResponse resp = c.execute(post);
+			Log.i(LOG_LABEL, LOG_MSG_PREFIX + "Executing post to " + SERVER + PATH);
+			Log.d(LOG_LABEL, LOG_MSG_PREFIX + "Spawning thread for HTTP post");
+			new Thread(new Runnable(){
 
-			//Return the HTTP response back to the user in the form of 
-			//a String
-			ByteArrayOutputStream bao = new ByteArrayOutputStream();
-			resp.getEntity().writeTo(bao);
-			String s = new String(bao.toByteArray());
-			Log.d(LOG_LABEL, "Response from server: " + s);
-			Log.v(LOG_LABEL, "Leaving HTTPPoster.doPost");
-			return s;
+				@Override
+				public void run() {
+					HttpResponse resp;
+					try {
+						resp = c.execute(post);
+						ByteArrayOutputStream bao = new ByteArrayOutputStream();
+						resp.getEntity().writeTo(bao);
+						Log.d(LOG_LABEL, LOG_MSG_PREFIX + "Response from server: " + new String(bao.toByteArray()));
+						
+					} catch (ClientProtocolException e) {
+						Log.e(LOG_LABEL, LOG_MSG_PREFIX + "ClientProtocolException executing post: " + e.getMessage());
+					} catch (IOException e) {
+						Log.e(LOG_LABEL, LOG_MSG_PREFIX + "IOException writing to ByteArrayOutputStream: " + e.getMessage());
+					} catch (Exception e){
+						Log.e(LOG_LABEL, LOG_MSG_PREFIX + "Other Exception of type:"+e.getClass());
+						Log.e(LOG_LABEL, LOG_MSG_PREFIX + "The message is: "+e.getMessage());
+					}
+				}
+
+			}).start();
+			Log.d(LOG_LABEL, LOG_MSG_PREFIX + "Thread for HTTP post started");
 
 		} 
 		catch (Exception e) {
-			Log.e("VUPHONE", "Other Exception of type:"+e.getClass());
-			Log.e("VUPHONE", "The message is: "+e.getMessage());
-			return null;
+			Log.e(LOG_LABEL, LOG_MSG_PREFIX + "Other Exception of type:"+e.getClass());
+			Log.e(LOG_LABEL, LOG_MSG_PREFIX + "The message is: "+e.getMessage());
+
 		}
 
+		Log.v(LOG_LABEL, LOG_MSG_PREFIX + "Leaving HTTPPoster.doAccidentPost");
 	}
 
 
