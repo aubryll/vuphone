1f6e56a0107f508119cd474de3b8f8b1f8220aa3
hamiltont <hamiltont@963311a8-3e45-11de-917f-4137a9cc17b9>
2009-06-04 13:57:08 +0000

    Fixed Testing UI bug, set up HTTPPoster to do an initial post with person info

--- trunk/projects/wreck-watch-android/src/org/vuphone/wwatch/android/Tabs.java
+++ trunk/projects/wreck-watch-android/src/org/vuphone/wwatch/android/Tabs.java
@@ -1,13 +1,31 @@
 package org.vuphone.wwatch.android;
 
+import org.vuphone.wwatch.android.http.HTTPPoster;
+
 import android.app.TabActivity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.widget.TabHost;
 
 public class Tabs extends TabActivity {
+	public final static String PREFS = "WreckWatchMainPrefs";
+	public final static String FIRST_LOAD = "FirstTimeLoading";
+	
 	public void onCreate(Bundle ice) {
 		super.onCreate(ice);
+		
+		SharedPreferences prefs = getSharedPreferences(PREFS, Context.MODE_PRIVATE);
+		
+		if (prefs.contains(FIRST_LOAD) == false)
+		{
+			Editor editor = prefs.edit();
+			editor.putBoolean(FIRST_LOAD, false);
+			editor.commit();
+			HTTPPoster.doInitialPost();
+		}
 		
 		TabHost th = getTabHost();
 		th.addTab(th.newTabSpec("Tab1").setIndicator("Settings").setContent(new Intent(this, SettingsUI.class)));
--- trunk/projects/wreck-watch-android/src/org/vuphone/wwatch/android/http/HTTPPoster.java
+++ trunk/projects/wreck-watch-android/src/org/vuphone/wwatch/android/http/HTTPPoster.java
@@ -30,11 +30,14 @@ import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.vuphone.wwatch.android.Waypoint;
 
+import android.provider.Settings;
 import android.util.Log;
 
-
-
 public class HTTPPoster {
+
+	// Chris's test server\\
+	// private static final String SERVER = "http://dorm.cmthompson.net";
+	// private static final String PATH = "/wreckwatch/test.php";
 
 
 	//Chris's test server\\
@@ -50,81 +53,143 @@ public class HTTPPoster {
 	private static final String LOG_LABEL = "VUPHONE";
 	private static final String LOG_MSG_PREFIX = "HTTPPoster: ";
 
-	//There might be a better way to do these methods....
+	// There might be a better way to do these methods....
 	/**
-	 * This method will be responsible for posting data to the
-	 * WreckWatch server.
+	 * This method will be responsible for posting data to the WreckWatch
+	 * server.
+	 * 
 	 * @param message
 	 */
-	public static void doAccidentPost(Long time, Double speed, Double dec, List<Waypoint> route){
+	public static void doAccidentPost(Long time, Double speed, Double dec,
+			List<Waypoint> route) {
 
-		try{
-			String timeStr = URLEncoder.encode(time.toString(), "UTF-8");
-			String speedStr = URLEncoder.encode(speed.toString(), "UTF-8");
-			String decStr = URLEncoder.encode(dec.toString(), "UTF-8");
+		String timeStr = Long.toString(time.longValue());
+		String speedStr = Double.toString(speed.doubleValue());
+		String decStr = Double.toString(dec.doubleValue());
+
+		try {
+			timeStr = URLEncoder.encode(time.toString(), "UTF-8");
+			speedStr = URLEncoder.encode(speed.toString(), "UTF-8");
+			decStr = URLEncoder.encode(dec.toString(), "UTF-8");
+		} catch (UnsupportedEncodingException use) {
+			Log.w(LOG_LABEL,
+					"HTTPPoster unable to encode one of the parameters");
+		}
+
+		Log.v(LOG_LABEL, LOG_MSG_PREFIX + "Entering HTTPPoster.doAccidentPost");
+		final HttpClient c = new DefaultHttpClient();
+		final HttpPost post = new HttpPost(SERVER + PATH);
+		post.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+		StringBuffer params = new StringBuffer();
+
+		// Create the parameter string
+		if (route != null) {
+			params.append("type=accident&user=thompchr%40gmail.com&time=" + timeStr + "&speed="
+					+ speedStr + "&dec=" + decStr + "&numpoints="
+					+ route.size());
 
 
-			Log.v(LOG_LABEL, LOG_MSG_PREFIX + "Entering HTTPPoster.doAccidentPost");
-			final HttpClient c = new DefaultHttpClient();
-			final HttpPost post = new HttpPost(SERVER + PATH);
-			post.addHeader("Content-Type","application/x-www-form-urlencoded");
 
-			StringBuffer params = new StringBuffer();
-
-			//Create the parameter string
-			if (route != null){
-				params.append("type=accident&user=thompchr%40gmail.com&time="+timeStr+"&speed="+speedStr+"&dec="+decStr+"&numpoints="+route.size());
-
-				for (int i = 0; i < route.size(); ++i){
-					params.append("&lat"+i+"="+route.get(i).getLatitude()+"&lon"+i+"="+route.get(i).getLongitude()+"&time"+i+"="+route.get(i).getTime());
+			String encodedRoute;
+			for (int i = 0; i < route.size(); ++i) {
+				encodedRoute = route.get(i).toString();
+				try {
+					encodedRoute = URLEncoder.encode(route.get(i).toString(), "UTF-8");
+				} catch (UnsupportedEncodingException use) {
+					Log.w(LOG_LABEL,
+					"HTTPPoster unable to encode one of the route objets");
 				}
-			}else{
+
+				params.append("&lat"+i+"="+route.get(i).getLatitude()+"&lon"+i+"="+route.get(i).getLongitude()+"&time"+i+"="+route.get(i).getTime());
+		
+			}
+		}else{
 				params.append("type=accident&user=thompchr%40gmail.com&time="+ timeStr +"&speed="+speedStr+"&dec="+decStr+"&numpoints=0");
+		}
+
+		// Add the parameters
+		Log
+				.v(LOG_LABEL, LOG_MSG_PREFIX + "Created parameter string: "
+						+ params);
+		post.setEntity(new ByteArrayEntity(params.toString().getBytes()));
+
+		// Do it
+		Log.i(LOG_LABEL, LOG_MSG_PREFIX + "Executing post to " + SERVER + PATH);
+		Log.d(LOG_LABEL, LOG_MSG_PREFIX + "Spawning thread for HTTP post");
+		new Thread(new Runnable() {
+
+			public void run() {
+				HttpResponse resp;
+				try {
+					resp = c.execute(post);
+					ByteArrayOutputStream bao = new ByteArrayOutputStream();
+					resp.getEntity().writeTo(bao);
+					Log.d(LOG_LABEL, LOG_MSG_PREFIX + "Response from server: "
+							+ new String(bao.toByteArray()));
+
+				} catch (ClientProtocolException e) {
+					Log.e(LOG_LABEL, LOG_MSG_PREFIX
+							+ "ClientProtocolException executing post: "
+							+ e.getMessage());
+				} catch (IOException e) {
+					Log.e(LOG_LABEL, LOG_MSG_PREFIX
+							+ "IOException writing to ByteArrayOutputStream: "
+							+ e.getMessage());
+				} catch (Exception e) {
+					Log.e(LOG_LABEL, LOG_MSG_PREFIX
+							+ "Other Exception of type:" + e.getClass());
+					Log.e(LOG_LABEL, LOG_MSG_PREFIX + "The message is: "
+							+ e.getMessage());
+				}
 			}
 
-
-
-			//Add the parameters
-			Log.v(LOG_LABEL, LOG_MSG_PREFIX + "Created parameter string: " + params);
-			post.setEntity(new ByteArrayEntity(params.toString().getBytes()));
-
-
-			//Do it
-			Log.i(LOG_LABEL, LOG_MSG_PREFIX + "Executing post to " + SERVER + PATH);
-			Log.d(LOG_LABEL, LOG_MSG_PREFIX + "Spawning thread for HTTP post");
-			new Thread(new Runnable(){
-
-				
-				public void run() {
-					HttpResponse resp;
-					try {
-						resp = c.execute(post);
-						ByteArrayOutputStream bao = new ByteArrayOutputStream();
-						resp.getEntity().writeTo(bao);
-						Log.d(LOG_LABEL, LOG_MSG_PREFIX + "Response from server: " + new String(bao.toByteArray()));
-						
-					} catch (ClientProtocolException e) {
-						Log.e(LOG_LABEL, LOG_MSG_PREFIX + "ClientProtocolException executing post: " + e.getMessage());
-					} catch (IOException e) {
-						Log.e(LOG_LABEL, LOG_MSG_PREFIX + "IOException writing to ByteArrayOutputStream: " + e.getMessage());
-					} catch (Exception e){
-						Log.e(LOG_LABEL, LOG_MSG_PREFIX + "Other Exception of type:"+e.getClass());
-						Log.e(LOG_LABEL, LOG_MSG_PREFIX + "The message is: "+e.getMessage());
-					}
-				}
-
-			}).start();
-			Log.d(LOG_LABEL, LOG_MSG_PREFIX + "Thread for HTTP post started");
-
-		} 
-		catch (Exception e) {
-			Log.e(LOG_LABEL, LOG_MSG_PREFIX + "Other Exception of type:"+e.getClass());
-			Log.e(LOG_LABEL, LOG_MSG_PREFIX + "The message is: "+e.getMessage());
-
-		}
+		}).start();
+		Log.d(LOG_LABEL, LOG_MSG_PREFIX + "Thread for HTTP post started");
 
 		Log.v(LOG_LABEL, LOG_MSG_PREFIX + "Leaving HTTPPoster.doAccidentPost");
 	}
+
+	public static void doInitialPost() {
+		final HttpClient c = new DefaultHttpClient();
+		final HttpPost post = new HttpPost(SERVER + PATH);
+		post.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+		StringBuffer params = new StringBuffer();
+		String aid = Settings.Secure.ANDROID_ID;
+		params.append("aid=" + aid);
+		
+		post.setEntity(new ByteArrayEntity(params.toString().getBytes()));
+
+		new Thread(new Runnable() {
+			public void run() {
+				HttpResponse resp;
+				try {
+					resp = c.execute(post);
+					ByteArrayOutputStream bao = new ByteArrayOutputStream();
+					resp.getEntity().writeTo(bao);
+					Log.d(LOG_LABEL, LOG_MSG_PREFIX + "Response from server: "
+							+ new String(bao.toByteArray()));
+
+				} catch (ClientProtocolException e) {
+					Log.e(LOG_LABEL, LOG_MSG_PREFIX
+							+ "ClientProtocolException executing post: "
+							+ e.getMessage());
+				} catch (IOException e) {
+					Log.e(LOG_LABEL, LOG_MSG_PREFIX
+							+ "IOException writing to ByteArrayOutputStream: "
+							+ e.getMessage());
+				} catch (Exception e) {
+					Log.e(LOG_LABEL, LOG_MSG_PREFIX
+							+ "Other Exception of type:" + e.getClass());
+					Log.e(LOG_LABEL, LOG_MSG_PREFIX + "The message is: "
+							+ e.getMessage());
+				}
+			}
+
+		}).start();
+	}
+
 	
 	public static void doContactUpdate(String deviceID, ArrayList<String> numbers){
 		Log.v(LOG_LABEL, LOG_MSG_PREFIX + "Entering HTTPPoster.doContactUpdate");
