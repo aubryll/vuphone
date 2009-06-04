b33fd2f09f9c596d002f8efc164d184ac3f94aea
krzysz.zienki <krzysz.zienki@963311a8-3e45-11de-917f-4137a9cc17b9>
2009-06-04 21:07:36 +0000

    Corrected a few bugs in ConfirmationDialog

--- trunk/projects/wreck-watch-android/src/org/vuphone/wwatch/android/ConfirmationDialog.java
+++ trunk/projects/wreck-watch-android/src/org/vuphone/wwatch/android/ConfirmationDialog.java
@@ -21,22 +21,22 @@ class ConfirmationDialog extends ProgressDialog implements
 
 	private static final int DEFAULT_MAX_TIME = 10;
 	
-	private int max_time_ = 1;
+	private int maxTime_ = 1;
 	private int time_ = 0;
 	private Timer timer_ = null;
 	private Vibrator vibrator_ = null;
 
 	private final TestingUI activity_;
+	
+	private TimerTask countdownTask_ = null;
 
 	public void onClick(DialogInterface dialog, int button) {
 		switch (button) {
 		case DialogInterface.BUTTON_POSITIVE:
 			this.fireAccidentIntent(true);
-			vibrator_.cancel();
 			break;
 		case DialogInterface.BUTTON_NEGATIVE:
 			this.fireAccidentIntent(false);
-			vibrator_.cancel();
 			break;
 		}
 	}
@@ -53,53 +53,55 @@ class ConfirmationDialog extends ProgressDialog implements
 		vibrator_ = (Vibrator) context
 				.getSystemService(Context.VIBRATOR_SERVICE);
 
-		max_time_ = context.getSharedPreferences(VUphone.PREFERENCES_FILE,
+		maxTime_ = context.getSharedPreferences(VUphone.PREFERENCES_FILE,
 				Context.MODE_PRIVATE).getInt(VUphone.TIMEOUT_TAG,
 				ConfirmationDialog.DEFAULT_MAX_TIME);
 
 		super.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-		super.setMax(max_time_);
+		super.setMax(maxTime_);
 		super.setTitle("Alert");
-		super
-				.setMessage("WreckWatch has detected a crash. Did an accident occur?");
+		super.setMessage("WreckWatch has detected a crash. Did an accident occur?");
 		super.setCancelable(false);
 
 		super.setButton(DialogInterface.BUTTON_POSITIVE, "Yes", this);
 		super.setButton(DialogInterface.BUTTON_NEGATIVE, "No", this);
 
 		super.setOnDismissListener(this);
+		
+		countdownTask_ = new TimerTask() {
+			public void run() {
+				// The countdown should be turned off once the user click
+				// a button
+				/*
+				if (timer_ == null) {
+					this.cancel();
+					ConfirmationDialog.this.vibrator_.cancel();
+					return;
+				}*/
+				ConfirmationDialog.this
+						.setProgress(ConfirmationDialog.this.time_);
+				ConfirmationDialog.this.time_++;
+				if (time_ > maxTime_) {
+					timer_.cancel();
+					this.cancel();
+					ConfirmationDialog.this.timeout();
+				}
+			}
+		};
 	}
 
 	public void show() {
 		super.show();
 		this.startCountdown();
-		vibrator_.vibrate(max_time_ * 1000);
+		vibrator_.vibrate(maxTime_ * 1000);
 	}
 
 	private void startCountdown() {
-		if (timer_ != null)
-			timer_.cancel();
+		timer_.cancel();
 		timer_ = new Timer();
 		time_ = 0;
 
-		timer_.scheduleAtFixedRate(new TimerTask() {
-			public void run() {
-				// The countdown should be turned off once the user click
-				// a button
-				if (timer_ == null) {
-					this.cancel();
-					ConfirmationDialog.this.vibrator_.cancel();
-					return;
-				}
-				ConfirmationDialog.this
-						.setProgress(ConfirmationDialog.this.time_);
-				ConfirmationDialog.this.time_++;
-				if (time_ > max_time_) {
-					timer_.cancel();
-					ConfirmationDialog.this.timeout();
-				}
-			}
-		}, 0, 1000);
+		timer_.scheduleAtFixedRate(countdownTask_, 0, 1000);
 	}
 
 	/**
@@ -111,9 +113,9 @@ class ConfirmationDialog extends ProgressDialog implements
 	 */
 	private void fireAccidentIntent(boolean accidentOccurred) {
 		// Stop the timer so that it doesn't fire again
+		countdownTask_.cancel();
+		vibrator_.cancel();
 		timer_.cancel();
-		timer_ = null; // Bad practice but its the easiest way to turn off
-		// the current countdown
 
 		Intent intent = new Intent(activity_,
 				org.vuphone.wwatch.android.GPService.class);
