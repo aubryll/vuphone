35fb9add6e92e7e92baa3ecb7eba14a5abd2dae7
thompchr <thompchr@963311a8-3e45-11de-917f-4137a9cc17b9>
2009-06-18 04:00:58 +0000

    Changed a small behavior in SqlConstructor to use transactions properly.

--- trunk/projects/wreck-watch-server/src/org/vuphone/wwatch/sql/SqlConstructor.java
+++ trunk/projects/wreck-watch-server/src/org/vuphone/wwatch/sql/SqlConstructor.java
@@ -16,9 +16,10 @@
 package org.vuphone.wwatch.sql;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+
+import javax.sql.DataSource;
 
 /**
  * This class is responsible for constructing the WreckWatch database if it does
@@ -30,6 +31,7 @@ import java.sql.SQLException;
 
 public class SqlConstructor {
 
+	private static DataSource ds_;
 	/**
 	 * Static method responsible for creating the database, building the tables
 	 * and establishing the relationships. After this method is called, the
@@ -42,52 +44,41 @@ public class SqlConstructor {
 	 * @throws SQLException
 	 */
 	public static void prepareDatabase() throws SQLException {
-		try {
-			Class.forName("org.sqlite.JDBC");
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		Connection db = DriverManager
-				.getConnection("jdbc:sqlite:wreckwatch.db");
-
+		Connection db = ds_.getConnection();
 		db.setAutoCommit(false);
 
 		String sql = "CREATE TABLE IF NOT EXISTS People ( "
-				+ "id INTEGER PRIMARY KEY ASC AUTOINCREMENT,"
+				+ "id INTEGER AUTO_INCREMENT PRIMARY KEY ,"
 				+ "AndroidID VARCHAR(10)," + "PhoneNumber VARCHAR(15),"
 				+ "FirstName VARCHAR(50)," + "LastName VARCHAR(50),"
 				+ "Email VARCHAR(100));";
 		PreparedStatement prep = db.prepareStatement(sql);
 		prep.execute();
-		db.commit();
 
 		sql = "CREATE TABLE IF NOT EXISTS Wreck ( "
-				+ "WreckID INTEGER PRIMARY KEY ASC AUTOINCREMENT,"
+				+ "WreckID INTEGER AUTO_INCREMENT PRIMARY KEY,"
 				+ "Person INTEGER REFERENCES People(id),"
 				+ "Lat DOUBLE NOT NULL," + "Lon DOUBLE NOT NULL,"
 				+ "Time DATE NOT NULL," + "LargestAccel DOUBLE NOT NULL);";
 		prep = db.prepareStatement(sql);
 		prep.execute();
-		db.commit();
 
 		sql = "CREATE TABLE IF NOT EXISTS EmergencyContacts ( "
 				+ "PersonId INTEGER REFERENCES People(id),"
 				+ "ContactId VARCHAR(15) NOT NULL);";
 		prep = db.prepareStatement(sql);
 		prep.execute();
-		db.commit();
 
 		sql = "CREATE TABLE IF NOT EXISTS Route ( "
-				+ "CoordID INTEGER PRIMARY KEY ASC AUTOINCREMENT,"
+				+ "CoordID INTEGER AUTO_INCREMENT PRIMARY KEY,"
 				+ "WreckID INTEGER REFERENCES Wreck(WreckID), "
 				+ "Lat DOUBLE NOT NULL," + "Lon DOUBLE NOT NULL,"
 				+ "Time DATE NOT NULL);";
 		prep = db.prepareStatement(sql);
 		prep.execute();
-		db.commit();
 
 		sql = "CREATE TABLE IF NOT EXISTS WreckImages ( "
-				+ "ImageID INTEGER PRIMARY KEY ASC AUTOINCREMENT,"
+				+ "ImageID INTEGER AUTO_INCREMENT PRIMARY KEY,"
 				+ "WreckID INTEGER REFERENCES Wreck(WreckID), FileName TEXT NOT NULL,"
 				+ "Time DATE NOT NULL);";
 		prep = db.prepareStatement(sql);
@@ -97,4 +88,11 @@ public class SqlConstructor {
 		db.close();
 	}
 
+	public void setDataConnection(DataSource ds){
+		ds_ = ds;
+	}
+	
+	public DataSource getDataConnection(){
+		return ds_;
+	}
 }
