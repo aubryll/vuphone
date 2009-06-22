2713946cae024677c01a60d522ea8b905854c4c7
krzysz.zienki <krzysz.zienki@963311a8-3e45-11de-917f-4137a9cc17b9>
2009-06-22 18:20:17 +0000

    Updated server code to only accept two corners of the map view.

--- projects/wreck-watch-server/src/org/vuphone/wwatch/inforeq/InfoParser.java
+++ projects/wreck-watch-server/src/org/vuphone/wwatch/inforeq/InfoParser.java
@@ -20,49 +20,26 @@ public class InfoParser {
 			throws InvalidFormatException {
 		
 		InfoNotification info = new InfoNotification();
-
-		int latE6;
-		double lat;
-
-		int lonE6;
-		double lon;
+		
+		double tlLon, tlLat, brLon, brLat;
 		try {
 
 			info.setTime(Long.parseLong(request.getParameter("maxtime")));
 			
-			latE6 = Integer.parseInt(request.getParameter("lattl"));
-			lat = (double) latE6;
-			lat = lat / 1E6;
-			lonE6 = Integer.parseInt(request.getParameter("lontl"));
-			lon = (double) lonE6;
-			lon = lon / 1E6;
-			info.setTopLeftCorner(lat, lon);
-
-			latE6 = Integer.parseInt(request.getParameter("lattr"));
-			lat = (double) latE6;
-			lat = lat / 1E6;
-			lonE6 = Integer.parseInt(request.getParameter("lontr"));
-			lon = (double) lonE6;
-			lon = lon / 1E6;
-			info.setTopRightCorner(lat, lon);
-
-			latE6 = Integer.parseInt(request.getParameter("latbl"));
-			lat = (double) latE6;
-			lat = lat / 1E6;
-			lonE6 = Integer.parseInt(request.getParameter("lonbl"));
-			lon = (double) lonE6;
-			lon = lon / 1E6;
-			info.setBottomLeftCorner(lat, lon);
-
-			latE6 = Integer.parseInt(request.getParameter("latbr"));
-			lat = (double) latE6;
-			lat = lat / 1E6;
-			lonE6 = Integer.parseInt(request.getParameter("lonbr"));
-			lon = (double) lonE6;
-			lon = lon / 1E6;
-			info.setBottomRightCorner(lat, lon);
-
+			tlLat = Double.parseDouble(request.getParameter("lattl")) / 1E6;
+			tlLon = Double.parseDouble(request.getParameter("lontl")) / 1E6;
+			
+			brLat = Double.parseDouble(request.getParameter("latbr")) / 1E6;
+			brLon = Double.parseDouble(request.getParameter("lonbr")) / 1E6;
+			
+			info.setBottomRightCorner(brLat, brLon);
+			info.setTopLeftCorner(tlLat, tlLon);
+			
+			info.setTopRightCorner(brLat, tlLon);
+			info.setBottomLeftCorner(tlLat, brLon);
+			
 			info.setRequest(request);
+			
 		} catch (Exception e) {
 			// If we get here, likely the parameters were wrong or not present
 			
