ed8019f4027620df8b0201d43257f22ea688a972
sma1Chan <sma1Chan@963311a8-3e45-11de-917f-4137a9cc17b9>
2009-06-22 18:50:43 +0000

    Moved code out of ImageHandler into ImageHandler, ImageNotification and ImageParser

--- projects/wreck-watch-server/src/org/vuphone/wwatch/media/incoming/ImageHandler.java
+++ projects/wreck-watch-server/src/org/vuphone/wwatch/media/incoming/ImageHandler.java
@@ -13,26 +13,32 @@ import java.util.logging.Logger;
 
 import javax.sql.DataSource;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.ServletInputStream;
-
 import org.vuphone.wwatch.notification.Notification;
 import org.vuphone.wwatch.notification.NotificationHandler;
+import org.vuphone.wwatch.notification.InvalidFormatException;
 
 public class ImageHandler implements NotificationHandler {
 
 	private static final Logger logger_ = Logger.getLogger(ImageHandler.class.getName());
 	private DataSource ds_;
 	private ImageParser parser_;
-	private static final String TIME = "time";
-	private static final String WRECKID = "wreckid";
-	private static final String CONTENT_TYPE = "image/jpeg";
 	private static final String FILE_EXTENSION = ".jpg";
 	private static int FILE_NAME_PREFIX = -1;
 	public static final String IMAGE_DIRECTORY = "images";
 
 	public Notification handle(Notification n) {
 
+		ImageNotification in;
+		ImageHandledNotification ihn = null;
+		
+		//first get the ImageNotification by parsing the Request.
+		try {
+			in = parser_.getImageNotification(n);
+		} catch (InvalidFormatException excp) {
+			logger_.log(Level.SEVERE, "Got InvalidFormatException: " + excp.getMessage());
+			return ihn;
+		}
+		
 		Connection db = null;
 
 		try {
@@ -43,7 +49,7 @@ public class ImageHandler implements NotificationHandler {
 					"Unable to initialize org.sqlite.JDBC");
 			logger_.log(Level.SEVERE,
 					" Unable to continue without a database, stopping");
-			return n;
+			return ihn;
 		}
 		
 		//get the initial value to use from the database from the imageId column
@@ -52,19 +58,8 @@ public class ImageHandler implements NotificationHandler {
 		{
 			getInitialFileNamePrefix(db);
 		}
-		
-		//get data from request
-		HttpServletRequest request = n.getRequest();
-		if (!isRequestValid(request))
-		{
-			logger_.log(Level.SEVERE, "Request didn't have needed parameters");
-			return n;
-		}
-		
-		long time, wreckId;
-		time = Long.parseLong(request.getParameter(ImageHandler.TIME));
-		wreckId = Long.parseLong(request.getParameter(ImageHandler.WRECKID));
 						
+		//create fileName
 		String fileName;
 		synchronized (ImageHandler.class)
 		{
@@ -72,85 +67,41 @@ public class ImageHandler implements NotificationHandler {
 			fileName = Integer.toString(ImageHandler.FILE_NAME_PREFIX) + ImageHandler.FILE_EXTENSION;
 		}
 
-		//put other data from request into database
+		//put data from ImageNotification into the database
 		// Insert wreck into database
 		try {
 			// Prepare the SQL statement
 			PreparedStatement prep = null; 
 			prep = db
 					.prepareStatement("INSERT INTO WreckImages (ImageId, WreckID, FileName, Time) VALUES (NULL, ?, ?, ?);");
-			prep.setLong(1, wreckId);
+			prep.setLong(1, in.getWreckId());
 			prep.setString(2, fileName);
-			prep.setDate(3, new Date(time));
+			prep.setDate(3, new Date(in.getTime()));
 			prep.execute();
 		} catch (SQLException e) {
 			logger_.log(Level.SEVERE, "Got SQLException when inserting into WreckImages table :" + e.getMessage());
-			return n;
+			return ihn;
 		}
 		
 		//put image as file on disk
 		File imageFile = new File(ImageHandler.IMAGE_DIRECTORY, fileName);
 		try {
 			FileOutputStream writer = new FileOutputStream(imageFile);
-			ServletInputStream input = request.getInputStream();
-			byte[] bytes = new byte[1024];
-			int read = -1;
-			while ((read = input.read (bytes)) > 0)
-			{
-				writer.write(bytes, 0, read);
-			}
+			writer.write(in.getBytes(), 0, in.getBytes().length - 1);
 			writer.close();
 		} catch (IOException excp) {
 			logger_.log(Level.SEVERE, "Got an IOException: " + excp.getMessage());
-			return n;
+			return ihn;
 		}
 		
-		// Parse the Notification and extract the AccidentReport
-//		ImageHandledNotificaiton report;
-//		try {
-//			report = parser_.getAccident(n);
-//		} catch (AccidentFormatException e1) {
-//			e1.printStackTrace();
-//			logger_.log(Level.SEVERE,
-//					"Unable to parse the Notification:");
-//			logger_.log(Level.SEVERE, " Notification was: ");
-//			logger_.log(Level.SEVERE, " " + n.toString());
-//			logger_.log(Level.SEVERE,
-//					" Unable to continue without AccidentReport, stopping");
-//			return n;
-//		}
-//		
-
-		ImageHandledNotification in = new ImageHandledNotification();
-		in.setTime(new Date(time));
-		in.setWreckId(wreckId);
-		in.setFileName(fileName);
-		return in;
+		//create and return a ImageHandledNotification showing the reques was handled.
+		ihn = new ImageHandledNotification();
+		ihn.setTime(new Date(in.getTime()));
+		ihn.setWreckId(in.getWreckId());
+		ihn.setFileName(fileName);
+		return ihn;
 	}
 	
-	public boolean isRequestValid(HttpServletRequest request)
-	{
-		boolean isValid = false;
-
-		if (request.getParameter(ImageHandler.TIME) == null)
-		{
-			logger_.log(Level.SEVERE, "Unable to get time from the request");
-		}
-		else if (request.getContentType() == null ||
-				!request.getContentType().equalsIgnoreCase(ImageHandler.CONTENT_TYPE))
-		{
-			logger_.log(Level.SEVERE, "Expected Content Type to be " + ImageHandler.CONTENT_TYPE + " not " + request.getContentType());
-		}
-		else if (request.getParameter(ImageHandler.WRECKID) == null)
-		{
-			logger_.log(Level.SEVERE, "Unable to get wreckid from the request");
-		}
-		else
-		{
-			isValid = true;
-		}
-		return isValid;
-	}
 	
 	public void setDataConnection(DataSource ds){
 		ds_ = ds;
