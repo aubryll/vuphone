554f8e9e23d3b593cdaf4299f9a7708affa107ff
krzysz.zienki <krzysz.zienki@963311a8-3e45-11de-917f-4137a9cc17b9>
2009-06-24 19:31:06 +0000

    Massive changes to the gallery code. Introduced a Dialog class that loads in the image and displays them in a grid.

--- trunk/projects/wreck-watch-android/src/org/vuphone/wwatch/android/mapview/ImageAdapter.java
+++ trunk/projects/wreck-watch-android/src/org/vuphone/wwatch/android/mapview/ImageAdapter.java
@@ -12,36 +12,42 @@ import org.vuphone.wwatch.android.http.HttpOperationListener;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.Gallery;
+import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ImageAdapter extends BaseAdapter implements HttpOperationListener{
 
-    private Context context_;
-    
-    private int wreckID_ = -1;
-
     private Bitmap[] images_ = {};
-    
-	private static final String LOG_PREFIX = "ImageAdapter: ";
-    
+	private Handler handler_ = new Handler();
+	
     public ImageAdapter(Context c, int id) {
-    	context_ = c;
-    	wreckID_ = id;
-    	HTTPGetter.doPictureGet(wreckID_, this);
+    	loadPictures(id);
     }
     
 	public void operationComplete(HttpResponse resp) {
 		Log.v(VUphone.tag, "ImageAdapter.operationComplete()");
 		images_ = parseImages(resp);
 		Log.v(VUphone.tag, "ImageAdapter set images_ array");
-	}  
+		
+		handler_.post(new Runnable() {
+			public void run() {
+				notifyDataSetChanged();
+			}
+		});
+	}
     
+	private void loadPictures(int id) {
+		HTTPGetter.doPictureGet(id, this);
+	}
+	
     private Bitmap[] parseImages(HttpResponse resp) {
     	HttpEntity ent = resp.getEntity();
     	int imgTotal = Integer.parseInt(resp.getHeaders("ImageCount")[0].getValue());
@@ -64,9 +70,7 @@ public class ImageAdapter extends BaseAdapter implements HttpOperationListener{
 	    			offset += numRead;
 	    		}
 	    		
-	    		// TODO - This is too indirect. Work on efficiency.
-	    		ByteArrayInputStream bis = new ByteArrayInputStream(data);
-	    		images[i] = new BitmapDrawable(bis).getBitmap();
+	    		images[i] = BitmapFactory.decodeByteArray(data, 0, data.length);
 	    		
 	    	}
     	} catch (IOException e) {
@@ -81,21 +85,25 @@ public class ImageAdapter extends BaseAdapter implements HttpOperationListener{
     }
 
     public Object getItem(int position) {
-        return position;
+        return null;
     }
 
     public long getItemId(int position) {
-        return position;
+        return 0;
     }
 
     public View getView(int position, View convertView, ViewGroup parent) {
-        ImageView i = new ImageView(context_);
-
-        i.setImageBitmap(images_[position]);
-        // The line below sets the size of the pictures you are displaying.
-        i.setLayoutParams(new Gallery.LayoutParams(225, 150));
-        i.setScaleType(ImageView.ScaleType.FIT_XY);
-
-        return i;
+        ImageView imageView;
+        if (convertView == null) {  // If it's not recycled, initialize some attributes
+            imageView = new ImageView(parent.getContext());
+            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+        imageView.setImageBitmap(images_[position]);
+        
+        return imageView;
     }  
 }
