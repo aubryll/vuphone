a34c6c4c8ee97ab3a588140b45bdb774dcd4ee66
krzysz.zienki <krzysz.zienki@963311a8-3e45-11de-917f-4137a9cc17b9>
2009-06-25 01:02:35 +0000

    Gave ImageManipulator static methods for image processing. The server will now uniformly scale and save an uploaded image to fit the predefined bounds. The device still downloads the full quality picture. I will change that soon.

--- projects/wreck-watch-server/src/org/vuphone/wwatch/media/incoming/ImageManipulator.java
+++ projects/wreck-watch-server/src/org/vuphone/wwatch/media/incoming/ImageManipulator.java
@@ -16,7 +16,9 @@
 package org.vuphone.wwatch.media.incoming;
 
 import java.awt.Graphics2D;
-import java.awt.RenderingHints;
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsEnvironment;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -24,26 +26,57 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class ImageManipulator {
-	BufferedImage img_;
+
+	private static final int MAX_WIDTH = 200;
+	private static final int MAX_HEIGHT = 200;
 	
-	public ImageManipulator(String path){
+	public static BufferedImage scaleDown(File file) {
+
+		GraphicsConfiguration gc = GraphicsEnvironment.getLocalGraphicsEnvironment()
+		.getDefaultScreenDevice().getDefaultConfiguration();
+		
+		// Load the image
+		BufferedImage img = null;
 		try {
-			img_ = ImageIO.read(new File(path));
+			img = ImageIO.read(file);
+		} catch (IOException e) {
+			e.printStackTrace();
+			// TODO - Change this.
+			throw new RuntimeException("File not found");
+		}
+		
+		int width = img.getWidth();
+		int height = img.getHeight();
+		int transparency = img.getColorModel().getTransparency();
+
+		// If current image is within acceptable bounds then return it
+		if (width <= MAX_WIDTH && height <= MAX_HEIGHT)
+			return img;
+		
+		// Else, figure out the smallest factor to scale the image to acceptable bounds
+		double multX = (double) width / MAX_WIDTH;
+		double multY = (double) height / MAX_HEIGHT;
+		
+		double scale = 1.0 / Math.max(multX, multY);
+		AffineTransform trans = AffineTransform.getScaleInstance(scale, scale);
+		
+		// Create the mini image surface and draw to it the scaled image
+		int miniWidth = (int) (width * scale);
+		int miniHeight = (int) (height * scale);
+		BufferedImage mini = gc.createCompatibleImage(miniWidth, miniHeight, transparency);
+		Graphics2D g2 = mini.createGraphics();
+		g2.drawRenderedImage(img, trans);
+		g2.dispose();
+		
+		return mini;
+	}
+	
+	public static void saveImage(BufferedImage img, File file) {
+		try {
+			// TODO - do not hard code this
+			ImageIO.write(img, "jpeg", file);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public BufferedImage resize(int w, int h){
-		if (img_ == null)
-			return null;
-		BufferedImage temp = new BufferedImage(w, h, img_.getType());
-		
-		Graphics2D g = temp.createGraphics();
-		g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-		g.drawImage(img_, 0, 0, w, h, 0, 0, img_.getWidth(), img_.getHeight(), null);
-		g.dispose();
-		return temp;
-	}
-
 }
