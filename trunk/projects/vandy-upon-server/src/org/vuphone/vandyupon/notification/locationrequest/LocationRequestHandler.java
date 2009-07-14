3403ed3b8b787efff21680ec0409f63b032cdd63
thompchr <thompchr@963311a8-3e45-11de-917f-4137a9cc17b9>
2009-07-14 20:47:45 +0000

    Created the basic structure and java classes for the VandyUpon server.  Added necessary libraries and got it to start without dying.  Does not actually handle anything at this point. Does not currently construct the proper database.

---
+++ trunk/projects/vandy-upon-server/src/org/vuphone/vandyupon/notification/locationrequest/LocationRequestHandler.java
@@ -0,0 +1,29 @@
+ /**************************************************************************
+ * Copyright 2009 Chris Thompson                                           *
+ *                                                                         *
+ * Licensed under the Apache License, Version 2.0 (the "License");         *
+ * you may not use this file except in compliance with the License.        *
+ * You may obtain a copy of the License at                                 *
+ *                                                                         *
+ * http://www.apache.org/licenses/LICENSE-2.0                              *
+ *                                                                         *
+ * Unless required by applicable law or agreed to in writing, software     *
+ * distributed under the License is distributed on an "AS IS" BASIS,       *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.*
+ * See the License for the specific language governing permissions and     *
+ * limitations under the License.                                          *
+ **************************************************************************/
+package org.vuphone.vandyupon.notification.locationrequest;
+
+import org.vuphone.vandyupon.notification.HandlerFailedException;
+import org.vuphone.vandyupon.notification.Notification;
+import org.vuphone.vandyupon.notification.NotificationHandler;
+
+public class LocationRequestHandler implements NotificationHandler {
+
+	@Override
+	public Notification handle(Notification n) throws HandlerFailedException {
+		return null;
+	}
+
+}
