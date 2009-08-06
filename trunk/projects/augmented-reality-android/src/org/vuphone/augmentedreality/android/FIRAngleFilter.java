527ba38c365ac621803ec1408f4eafbddcf6e0ee
krzysz.zienki <krzysz.zienki@963311a8-3e45-11de-917f-4137a9cc17b9>
2009-08-06 22:22:36 +0000

    Fixed a simple bug in the grapher.

--- trunk/projects/augmented-reality-android/src/org/vuphone/augmentedreality/android/FIRAngleFilter.java
+++ trunk/projects/augmented-reality-android/src/org/vuphone/augmentedreality/android/FIRAngleFilter.java
@@ -19,27 +19,77 @@ package org.vuphone.augmentedreality.android;
 public class FIRAngleFilter {
 
 	private final int capacity_;
-	private int size_; // Number of elements (<= capacity_)
-	private int index_; // Index of the oldest reading
-	private final float[] buffer_;
+	public int size_; 				// Number of elements (<= capacity_)
+	private int index_; 			// Index of the oldest reading
+	private final float[] buffer_;	// Holds the raw angles
+	private final float[] coeffs_;	// Holds the weights of each angle
+	private float unfiltered_;
 
 	public FIRAngleFilter(int cap) {
 		capacity_ = cap;
 		size_ = index_ = 0;
 		buffer_ = new float[capacity_];
+		coeffs_ = new float[capacity_];
 	}
 
 	public void add(float angle) {
+		unfiltered_ = angle;
+		// Calculate the coefficient for this value
+		
+		float mean = getMean();
+		float dev = getStdDev();
+		float z = (angle - mean) / dev;
+		if (z > 1.5 && z < -1.5)
+			return;
+		//float c = 1 / z;
+		
 		if (size_ == capacity_) {
 			buffer_[index_] = angle;
+			//coeffs_[index_] = c;
 			index_ = (index_ + 1) % capacity_;
 			return;
 		}
 
 		buffer_[(index_ + size_) % capacity_] = angle;
+		//coeffs_[(index_ + size_) % capacity_] = c;
 		// TODO - Above offset evaluates to index_ if were full so we can
 		// combine with the if above
 		size_++;
+	}
+	
+	public float getAngle() {
+		return getMean();
+	}
+	
+	public float getUnfiltered() {
+		return unfiltered_;
+	}
+	
+	private float getMean() {
+		if (size_ == 0)
+			return 0;
+		
+		float sum = 0;
+		for (int i = 0; i < size_; i++) {
+			sum += buffer_[(index_ + i) % capacity_];
+		}
+
+		return sum / size_;
+	}
+	
+	private float getStdDev() {
+		if (size_ == 0)
+			return 0;
+		
+		float sumSqrd = 0;
+		float mean = getMean();
+		
+		for (int i = 0; i < size_; i++) {
+			float angle = buffer_[(index_ + i) % capacity_]; 
+			sumSqrd += (angle - mean) * (angle - mean);
+		}
+		
+		return (float) Math.sqrt(sumSqrd / size_);
 	}
 
 	public String dump() {
@@ -54,11 +104,16 @@ public class FIRAngleFilter {
 		if (size_ == 0)
 			return "Empty";
 
+		float mean = getMean();
+		float dev = getStdDev();
+		
 		String str = "";
 		for (int i = 0; i < size_; i++) {
-			str += ", " + buffer_[(index_ + i) % capacity_];
+			float angle = buffer_[(index_ + i) % capacity_];
+			float z = (angle - mean) / dev;
+			str += ", " + angle + ": " + z;
 		}
 
-		return "{" + str.substring(2) + "}";
+		return "{" + str.substring(2) + "} u = " + mean + " s = " + dev;
 	}
 }
