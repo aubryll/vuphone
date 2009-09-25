7440aadc797b0b90f5fc8a1956cf35121ce2d8c8
aaroneous88 <aaroneous88@963311a8-3e45-11de-917f-4137a9cc17b9>
2009-09-25 18:42:49 +0000

    Started unit testing

---
+++ trunk/projects/vandy-upon-iphone/Tests/EventStoreTests.m
@@ -0,0 +1,50 @@
+//
+//  VUEventTestCase.m
+//  VandyUpon
+//
+//  Created by Aaron Thompson on 9/7/09.
+//  Copyright 2009 Iostudio, LLC. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+#import "RemoteEventLoader.h"
+#import "EventStore.h"
+
+@interface EventStoreTests : SenTestCase {
+}
+
+@end
+
+@implementation EventStoreTests
+
+/* The setUp method is called automatically before each test-case method (methods whose name starts with 'test').
+ */
+- (void)setUp {
+	NSLog(@"setUp");
+}
+
+/* The tearDown method is called automatically after each test-case method (methods whose name starts with 'test').
+ */
+- (void)tearDown {
+	NSLog(@"tearDown");
+}
+
+- (void)testPass {
+	STAssertTrue(true, @"Pass!");
+}
+
+- (void)testEventStore
+{
+	EventStore *store = [EventStore sharedEventStore];
+	STAssertNotNil(store, @"Shared event store not nil");
+	
+	NSPersistentStoreCoordinator *coordinator = [store persistentStoreCoordinator];
+	STAssertNotNil(coordinator, @"Persistent store coordinator not nil");
+	
+	NSManagedObjectModel *model = [store managedObjectModel];
+	STAssertNotNil(model, @"Managed object model not nil");
+}
+
+@end
