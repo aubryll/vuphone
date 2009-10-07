ea53ce78ceb32b2c2d1cf77c8ed84c65eddb086e
ben <ben@wibking.com>
2009-10-07 03:06:11 +0000

    copyright change

--- projects/campusmaps-iphone/main.m
+++ projects/campusmaps-iphone/main.m
@@ -3,7 +3,7 @@
 //  CampusMaps
 //
 //  Created by Aaron Thompson on 10/6/09.
-//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//  Copyright VUMAD 2009. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
