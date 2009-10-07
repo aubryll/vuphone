8e623219ae6931370ba8dfb6573fe03d2e7d2ce0
aaroneous88 <aaroneous88@963311a8-3e45-11de-917f-4137a9cc17b9>
2009-10-07 01:54:43 +0000

    Created CampusMaps Xcode project

---
+++ projects/campusmaps-iphone/Classes/CampusMapsAppDelegate.h
@@ -0,0 +1,27 @@
+//
+//  CampusMapsAppDelegate.h
+//  CampusMaps
+//
+//  Created by Aaron Thompson on 10/6/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+@interface CampusMapsAppDelegate : NSObject <UIApplicationDelegate> {
+
+    NSManagedObjectModel *managedObjectModel;
+    NSManagedObjectContext *managedObjectContext;	    
+    NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+    UIWindow *window;
+}
+
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+- (NSString *)applicationDocumentsDirectory;
+
+@end
+
