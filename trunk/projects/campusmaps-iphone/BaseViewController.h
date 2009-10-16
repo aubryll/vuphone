8fdff9c00c9550c86a9e1d611dd08f921181be52
josh.a.stein <josh.a.stein@963311a8-3e45-11de-917f-4137a9cc17b9>
2009-10-16 05:14:39 +0000

    The new program model is that the BaseViewController is responsible for loading the MapView and ListView.
    -Some things are broken:
    
    there is a menu bar sized (20 pixel) gap at the top
    second run crashes (build and go will launch the app properly)
    list view is not implemented
    the functionality of the flip view button is not implemented

---
+++ trunk/projects/campusmaps-iphone/BaseViewController.h
@@ -0,0 +1,25 @@
+//
+//  BaseView.h
+//  CampusMaps
+//
+//  Created by Joshua Stein on 10/15/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MapViewController.h"
+#import "ListViewController.h"
+
+
+@interface BaseViewController : UIViewController {
+
+	IBOutlet MapViewController *mapViewController;
+	IBOutlet ListViewController *listViewController;
+	
+	UIButton * flipViewButton;
+	
+}
+
+-(void)instantiateFlipViewButton;
+
+@end
