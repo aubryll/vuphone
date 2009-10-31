ac56a629683862d9f4b19687cf2b781fce00ea5b
corespace <corespace@963311a8-3e45-11de-917f-4137a9cc17b9>
2009-10-31 22:58:10 +0000

    Added Licenses to all code files.

--- trunk/projects/campusmaps-android/src/edu/vanderbilt/vuphone/android/campusmaps/Building.java
+++ trunk/projects/campusmaps-android/src/edu/vanderbilt/vuphone/android/campusmaps/Building.java
@@ -1,3 +1,21 @@
+/**
+ * Android Campus Maps
+ *  http://code.google.com/p/vuphone/
+ * 
+ * @author Adam Albright
+ * @date Oct 16, 2009
+ * 
+ * Copyright 2009 VUPhone Team
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License. 
+ *  You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software 
+ *  distributed under the License is distributed on an "AS IS" BASIS, 
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ *  implied. See the License for the specific language governing 
+ *  permissions and limitations under the License. 
+ */
+
 package edu.vanderbilt.vuphone.android.campusmaps;
 
 import com.google.android.maps.GeoPoint;
--- trunk/projects/campusmaps-android/src/edu/vanderbilt/vuphone/android/campusmaps/BuildingList.java
+++ trunk/projects/campusmaps-android/src/edu/vanderbilt/vuphone/android/campusmaps/BuildingList.java
@@ -1,3 +1,21 @@
+/**
+ * Android Campus Maps
+ *  http://code.google.com/p/vuphone/
+ * 
+ * @author Adam Albright
+ * @date Oct 16, 2009
+ * 
+ * Copyright 2009 VUPhone Team
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License. 
+ *  You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing, software 
+ *  distributed under the License is distributed on an "AS IS" BASIS, 
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
+ *  implied. See the License for the specific language governing 
+ *  permissions and limitations under the License. 
+ */
+
 package edu.vanderbilt.vuphone.android.campusmaps;
 
 import java.util.ArrayList;
@@ -17,20 +35,15 @@ import com.google.android.maps.GeoPoint;
 public class BuildingList extends ListActivity {
 
 	// List of buildings -- should get populated at boot
-	private HashMap<Integer, Building> building_ = null;
+	private HashMap<Integer, Building> buildings_ = null;
 	private HashMap<Integer, Integer> indexToHash_ = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
-		Bundle extras = getIntent().getExtras();
-		if (extras != null) {
-			// Parse any input parameters
-		}
-
-		building_ = SharedData.getInstance().getBuildingList();
-		if (building_ == null || building_.size() < 1) {
+		buildings_ = SharedData.getInstance().getBuildingList();
+		if (buildings_ == null || buildings_.size() < 1) {
 			echo("Building list not found!");
 		} else {
 			// TODO Create custom adapter so we can show more than just the
@@ -39,7 +52,7 @@ public class BuildingList extends ListActivity {
 					android.R.layout.simple_list_item_1, getBuildingList(null)));
 		}
 
-		Log.d("mad", "Buildings loaded: " + building_.size());
+		Log.d("mad", "Buildings loaded: " + buildings_.size());
 	}
 
 	@Override
@@ -47,7 +60,7 @@ public class BuildingList extends ListActivity {
 		super.onListItemClick(l, v, position, id);
 
 		// Get the clicked building
-		Building b = building_.get(indexToHash_.get(position));
+		Building b = buildings_.get(indexToHash_.get(position));
 
 		// TODO open a menu that asks what they want to do
 
@@ -71,7 +84,7 @@ public class BuildingList extends ListActivity {
 
 		// Extract the building names for now
 		ArrayList<String> list = new ArrayList<String>();
-		Iterator<Building> i = building_.values().iterator();
+		Iterator<Building> i = buildings_.values().iterator();
 		indexToHash_ = new HashMap<Integer, Integer>();
 
 		int index = 0;
