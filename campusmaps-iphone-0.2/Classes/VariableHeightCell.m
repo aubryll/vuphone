81ae6da0cdaa3c95183b301767d747ab5e0cc4c8
demetri.miller <demetri.miller@963311a8-3e45-11de-917f-4137a9cc17b9>
2009-12-23 17:40:25 +0000

    - added new classes. No other changes - Details view now displays text appropriately, but broken.

---
+++ campusmaps-iphone-0.2/Classes/VariableHeightCell.m
@@ -0,0 +1,49 @@
+//
+//  VariableHeightCell.m
+//  CampusMaps
+//
+//  Created by Demetri Miller on 12/23/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "VariableHeightCell.h"
+
+
+@implementation VariableHeightCell
+
+@synthesize textView;
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    if (self = [super initWithStyle:style reuseIdentifier:reuseIdentifier]) {
+        // Initialization code
+		textView = [[UITextView alloc] initWithFrame:CGRectMake(16.0, 4.0, 280, 36.0)];
+		textView.editable = NO;
+		textView.font = [UIFont systemFontOfSize:13.0];
+		[self addSubview:textView];
+    }
+    return self;
+}
+
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+
+- (void)setText:(NSString *)text {
+	self.textView.text = text;
+	CGRect contentRect = self.textView.frame;
+	contentRect.size.height = self.textView.contentSize.height;
+	self.textView.frame = contentRect;
+}
+
+- (void)dealloc {
+	self.textView = nil;
+    [super dealloc];
+}
+
+
+@end
