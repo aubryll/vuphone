a29b50d06808547ec51bf04828579c14f074142f
demetri.miller <demetri.miller@963311a8-3e45-11de-917f-4137a9cc17b9>
2009-12-23 17:40:25 +0000

    - added new classes. No other changes - Details view now displays text appropriately, but broken.

---
+++ branches/projects/campusmaps-iphone-0.2/Classes/VariableHeightCell.h
@@ -0,0 +1,20 @@
+//
+//  VariableHeightCell.h
+//  CampusMaps
+//
+//  Created by Demetri Miller on 12/23/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface VariableHeightCell : UITableViewCell {
+	UITextView *textView;
+}
+
+- (void)setText:(NSString *)text;
+
+@property(retain) UITextView *textView;
+
+@end
