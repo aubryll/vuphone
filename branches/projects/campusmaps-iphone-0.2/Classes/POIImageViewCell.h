9c6c50b75ea3e0a981a007154703531c8a65f15d
demetri.miller <demetri.miller@963311a8-3e45-11de-917f-4137a9cc17b9>
2009-12-24 06:52:50 +0000

    - Abstracted poi image to class POIImageViewCell

---
+++ branches/projects/campusmaps-iphone-0.2/Classes/POIImageViewCell.h
@@ -0,0 +1,30 @@
+//
+//  POIImageViewCell.h
+//  CampusMaps
+//
+//  Created by Demetri Miller on 12/24/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface POIImageViewCell : UITableViewCell {
+	UIImageView *poiImage;
+	
+	// Background for the image to be displayed. We need
+	// this to be transparent.
+	UIView *backView;
+}
+
+-(void)setupImage:(UIImage *)image;
+
+-(CGFloat) getImageHeight:(UIImageView *) image;
+-(CGFloat) getImageWidth:(UIImageView *) image;
+
+// Returns the offset needed to evenly space the image horizontally.
+-(CGFloat) getImageOffset:(UIImageView *) image;
+
+@property(retain) UIImageView *poiImage;
+@property(retain) UIView *backView;
+@end
