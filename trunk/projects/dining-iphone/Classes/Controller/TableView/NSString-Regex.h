eb1ddaf01da8dcc32bc7ff3156baf12837f56b84
aaroneous88 <aaroneous88@963311a8-3e45-11de-917f-4137a9cc17b9>
2010-01-22 01:27:14 +0000

    Fixed hour calculations (now supports times going past minutes) Added limited support for Chef James menus

---
+++ trunk/projects/dining-iphone/Classes/Controller/TableView/NSString-Regex.h
@@ -0,0 +1,16 @@
+//
+//  NSString-Regex.h
+//  Dining
+//
+//  Created by Aaron Thompson on 1/21/10.
+//  Copyright 2010 Vanderbilt University. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface NSString (Regex)
+
+- (NSString *)stripMedia;
+
+@end
