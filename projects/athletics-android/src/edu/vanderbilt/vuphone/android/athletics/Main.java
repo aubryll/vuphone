c62391c31cfaa7f3015bba8fb5ea4271a7e9d904
sto2979 <sto2979@963311a8-3e45-11de-917f-4137a9cc17b9>
2010-02-04 01:07:08 +0000

    ListView

--- projects/athletics-android/src/edu/vanderbilt/vuphone/android/athletics/Main.java
+++ projects/athletics-android/src/edu/vanderbilt/vuphone/android/athletics/Main.java
@@ -1,13 +1,19 @@
 package edu.vanderbilt.vuphone.android.athletics;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
-public class Main extends Activity {
+public class Main extends ListActivity {
     /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	  super.onCreate(savedInstanceState);
+	  
+	  setListAdapter(new ArrayAdapter<String>(this,
+	          android.R.layout.simple_list_item_1, SPORTS));
+	  getListView().setTextFilterEnabled(true);
+	}
+	
+	static final String[] SPORTS = new String[] {"Football"};
 }
