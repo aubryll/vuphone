fdf77bb2204f34e8a544de1a77e51017157d1940
urhereimnot <urhereimnot@963311a8-3e45-11de-917f-4137a9cc17b9>
2010-02-08 23:59:47 +0000

    Skeleton class. copied from EventRating

---
+++ projects/events-server/src/org/vuphone/vandyupon/notification/diningpost/DiningPostRating.java
@@ -0,0 +1,58 @@
+
+package org.vuphone.vandyupon.notification.diningpost;
+
+import java.util.ArrayList;
+
+import org.vuphone.vandyupon.datastructs.Rating;
+
+public class DiningPostRating extends Rating {
+
+	private ArrayList<EventRatingContainer> ratings_;
+	private long totalUp_ = 0;
+	private long totalDown_ = 0;
+	private double avg_ = 0;
+	private boolean finalizeCalled_ = false;
+
+	public DiningPostRating() {
+		super("event");
+		ratings_ = new ArrayList<EventRatingContainer>();
+	}
+
+	public void addRating(long user, int value, String comment, long submissionDate){
+		ratings_.add(new EventRatingContainer(user, value, comment, submissionDate));
+	}
+
+	public void finalize(){
+		if (!finalizeCalled_){
+			for (EventRatingContainer erc:ratings_){
+				if (erc.getValue() == 1)
+					++totalUp_;
+				else
+					++totalDown_;
+			}
+			avg_ = (totalUp_ + totalDown_)/(double)ratings_.size();
+		}
+	}
+	
+	public boolean finalizeCalled(){
+		return finalizeCalled_;
+	}
+	
+	public double getAverage(){
+		return avg_;
+	}
+	
+	public long getUp(){
+		return totalUp_;
+	}
+	
+	public long getDown(){
+		return totalDown_;
+	}
+
+	@Override
+	public Object getRating() {
+		return ratings_;
+	}
+
+}
