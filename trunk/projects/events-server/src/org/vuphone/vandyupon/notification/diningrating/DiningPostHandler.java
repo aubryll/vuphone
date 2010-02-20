310495284c9f5f064f108e68939ea57743355cfc
michael.j.t.fagan <michael.j.t.fagan@963311a8-3e45-11de-917f-4137a9cc17b9>
2010-02-20 19:06:39 +0000

    Commiting Handler for meeting, not finished or commented!

--- trunk/projects/events-server/src/org/vuphone/vandyupon/notification/diningrating/DiningPostHandler.java
+++ trunk/projects/events-server/src/org/vuphone/vandyupon/notification/diningrating/DiningPostHandler.java
@@ -15,9 +15,90 @@ import org.vuphone.vandyupon.notification.InvalidFormatException;
 import org.vuphone.vandyupon.notification.Notification;
 import org.vuphone.vandyupon.notification.NotificationHandler;
 import org.vuphone.vandyupon.notification.ResponseNotification;
+import org.vuphone.vandyupon.notification.diningpost.DiningPost;
+import org.vuphone.vandyupon.notification.eventpost.EventPost;
 
 public class DiningPostHandler implements NotificationHandler {
 	
+	private DataSource ds_;
 	
+	public DataSource getDataConnection() {
+		return ds_;
+	}
+	
+	private int addRating(DiningPost dp) throws SQLException {
+		String sql;
+		
+		Connection conn = ds_.getConnection();
+		
+		sql = "INSERT INTO ratings (deviceid, loc, rating) VALUES (?, ?, ?)";
+		
+		PreparedStatement prep = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+		prep.setString(1, dp.getDeviceID());
+		prep.setInt(2, dp.getLocation());
+		prep.setLong(3, dp.getRating());
+		
+		ResultSet rs = prep.executeQuery();
+		rs.next();
+		
+		if (prep.executeUpdate() == 0) {
+			throw new SQLException(
+					"Insertion into vandyupon.dining failed for an unknown reason");
+		} else/*Everything Worked*/ {
+			rs = prep.getGeneratedKeys();
+			rs.next();
+			int id = rs.getInt(1);
+			prep.execute();
+			return id;
+		}
+		
+	}
+	
+	// This method is used to see if the request is already in the Database
+	private boolean checkForEntry(DiningPost dp) throws SQLException {
+		
+		String sql;
+		
+		Connection conn = ds_.getConnection();
+		
+		sql = "SELECT * FROM people WHERE deviceid LIKE ? AND loc LIKE ?";
+		
+		PreparedStatement prep = conn.prepareStatement(sql,
+				Statement.RETURN_GENERATED_KEYS);
+		
+		prep.setString(1, dp.getDeviceID());
+		
+		Integer location = new Integer(dp.getLocation());
+		String loc = location.toString();
+		prep.setString(2, loc);
+		
+		ResultSet rs = prep.executeQuery();
+		
+		try{
+			rs.next();
+			int exists = rs.getInt(1);
+			if (exists != 0) {
+				rs.close();
+				return true;
+			} else/* The ID is not in the database with the current location*/ {
+				rs.close();
+				return false;
+			}
+		} catch (SQLException e) {
+			return false;
+		}
+	}
+	
+	
+	public ResponseNotification handle(Notification n) throws HandlerFailedException {
+		if (!(n instanceof DiningPost)) {
+			HandlerFailedException hfe = new HandlerFailedException();
+			hfe.initCause(new InvalidFormatException());
+			throw hfe;
+		}
+		
+		DiningPost dp = (DiningPost) n;
+		
+	}
 
 }
