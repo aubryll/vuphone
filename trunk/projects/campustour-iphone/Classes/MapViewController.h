f887fdd4ac2dfec9580f3ea7aea29c34c6c85ce5
sma1Chan <sma1Chan@963311a8-3e45-11de-917f-4137a9cc17b9>
2010-02-23 05:28:04 +0000

    added code to display waypoints currently through non KissXML parser

--- trunk/projects/campustour-iphone/Classes/MapViewController.h
+++ trunk/projects/campustour-iphone/Classes/MapViewController.h
@@ -8,12 +8,28 @@
 
 #import <UIKit/UIKit.h>
 #import <MapKit/MapKit.h>
+#import <Waypoint.h>
 
-@interface MapViewController : UIViewController {
+#define ANNOTATION_IMAGE_FILE @"Images/annotation_v.png"
+#define POI_REQUEST_ALTERNATIVE @"waypoints-db.xml"
+
+enum XMLSTATE {
+	NOTHING,
+	DESCRIPTION,
+	AUDIOPATH
+};
+
+@interface MapViewController : UIViewController <MKMapViewDelegate> {
 
 	IBOutlet MKMapView* mapView;
-
+	UIImage* annotationImage;
+	Waypoint *tmpWaypoint;
+	enum XMLSTATE lookingFor;
 }
 
 - (IBAction)centerOnCampus:(id)sender;
+//temporary parser code.
+- (void)parser:(NSXMLParser *)parser didEndElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName;
+-(void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qualifiedName  attributes:(NSDictionary *)attributeDict;
+- (void)parser:(NSXMLParser *)parser foundCharacters:(NSString *)string;
 @end
