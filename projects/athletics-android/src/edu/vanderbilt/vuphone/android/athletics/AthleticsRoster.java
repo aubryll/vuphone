e94f83b30ea69f6308091d830fe5286817b498f0
russ.amos <russ.amos@963311a8-3e45-11de-917f-4137a9cc17b9>
2010-02-24 23:57:25 +0000

    Making the athletics roster prettier.

--- projects/athletics-android/src/edu/vanderbilt/vuphone/android/athletics/AthleticsRoster.java
+++ projects/athletics-android/src/edu/vanderbilt/vuphone/android/athletics/AthleticsRoster.java
@@ -1,24 +1,40 @@
 package edu.vanderbilt.vuphone.android.athletics;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import android.app.ListActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.widget.SimpleAdapter;
 
 public class AthleticsRoster extends ListActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.roster);
 		
-		ArrayList<String> roster = new ArrayList<String>();
-		roster.add("Larry");
-		roster.add("Adam");
+		ArrayList<HashMap<String, String>> roster = new 
+			ArrayList<HashMap<String, String>>();
 
-		setListAdapter(new ArrayAdapter<String>(this,
-			android.R.layout.simple_list_item_1, roster));
+		HashMap<String, String> m = new HashMap<String, String>();
+		m.put("name", "Larry");
+		m.put("position", "QB");
+		m.put("number", "1");
+		
+		HashMap<String, String> m2 = new HashMap<String, String>();
+		m2.put("name", "Adam");
+		m2.put("position", "TE");
+		m2.put("number", "2");
+
+		roster.add(m);
+		roster.add(m2);
+
+		String[] columns = {"number", "position", "name"};
+		int[] column_ids = {R.roster_entry.number,
+			R.roster_entry.position, R.roster_entry.name};
+		setListAdapter(new SimpleAdapter(this, roster,
+			R.layout.roster_entry, columns, column_ids));
 	}
-
+	
 }
-
