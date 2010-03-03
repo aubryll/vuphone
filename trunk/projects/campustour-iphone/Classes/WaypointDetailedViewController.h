d2962e689d89b949e97c2087cdde427fa60f33c8
sma1Chan <sma1Chan@963311a8-3e45-11de-917f-4137a9cc17b9>
2010-03-03 02:33:30 +0000

    adding WaypointDetailedViewController to SVN.

---
+++ trunk/projects/campustour-iphone/Classes/WaypointDetailedViewController.h
@@ -0,0 +1,23 @@
+//
+//  WaypointDetailedViewController.h
+//  Campus Tour
+//
+//  Created by sma1 on 2/23/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Waypoint.h>
+
+
+@interface WaypointDetailedViewController : UIViewController {
+
+	IBOutlet UITextView *description;
+	IBOutlet UILabel *theName;
+	Waypoint *waypoint;
+}
+
+@property (retain) Waypoint *waypoint;
+
+
+@end
---
+++ trunk/projects/campustour-iphone/Classes/WaypointDetailedViewController.m
@@ -0,0 +1,59 @@
+//
+//  WaypointDetailedViewController.m
+//  Campus Tour
+//
+//  Created by sma1 on 2/23/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "WaypointDetailedViewController.h"
+
+
+@implementation WaypointDetailedViewController
+
+@synthesize waypoint;
+
+ // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
+//- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+//    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
+//        // Custom initialization
+//		self.title = @"Detailed Information";
+//    }
+//    return self;
+//}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+	[super viewWillAppear:animated];
+	self.title = self.waypoint.name;
+	theName.text = self.waypoint.name;
+	description.text = self.waypoint.description;
+}
+
+/*
+// Override to allow orientations other than the default portrait orientation.
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+*/
+
+- (void)didReceiveMemoryWarning {
+	// Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+	
+	// Release any cached data, images, etc that aren't in use.
+}
+
+- (void)viewDidUnload {
+	// Release any retained subviews of the main view.
+	// e.g. self.myOutlet = nil;
+}
+
+
+- (void)dealloc {
+	self.waypoint = nil;
+    [super dealloc];
+}
+
+@end
