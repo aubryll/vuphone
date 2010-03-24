153966a91e7fa1b82abd154bd9279c81231ce5e3
michael.j.t.fagan <michael.j.t.fagan@963311a8-3e45-11de-917f-4137a9cc17b9>
2010-03-24 04:22:57 +0000

    Added validity checks for Location and Rating in DiningHandler. Added a bunch of comments.

--- trunk/projects/events-server/src/org/vuphone/vandyupon/notification/diningrating/DiningHandler.java
+++ trunk/projects/events-server/src/org/vuphone/vandyupon/notification/diningrating/DiningHandler.java
@@ -20,7 +20,7 @@ import org.vuphone.vandyupon.notification.ResponseNotification;
 public class DiningHandler implements NotificationHandler {
 	
 	
-	// The datasource, whatever that is!
+	// The datasource, aka the Database connection
 	private DataSource ds_;
 	
 	// This method gets the SQL connection.
@@ -28,69 +28,81 @@ public class DiningHandler implements NotificationHandler {
 		return ds_;
 	}
 	
+	// This method sets the datasource
 	public void setDataConnection(DataSource ds) {
 		ds_ = ds;
 	}
 	
 	// This method adds a rating to the database.
 	private int addRating(DiningRating dp) throws SQLException {
-		String sql;
-		Connection conn = ds_.getConnection();
-		
-		sql = "INSERT INTO DiningRatings (deviceid, loc, rating) VALUES (?, ?, ?)";
-		
-		PreparedStatement prep = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-		prep.setString(1, dp.getDeviceID());
-		prep.setInt(2, dp.getLocation());
-		prep.setInt(3, dp.getRating());
-
-		
-		if (prep.executeUpdate() == 0) {
-			throw new SQLException(
-					"Insertion into vandyupon.dining failed for an unknown reason");
+		if((checkLocation(dp.getLocation()))&&(checkRating(dp.getRating()))){
+			String sql;
+			Connection conn = ds_.getConnection();
+			
+			// Inserts a rating into the DiningRatings Table
+			sql = "INSERT INTO DiningRatings (deviceid, loc, rating) VALUES (?, ?, ?)";
+			
+			// This block puts the passed values into the above statement
+			PreparedStatement prep = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			prep.setString(1, dp.getDeviceID());
+			prep.setInt(2, dp.getLocation());
+			prep.setInt(3, dp.getRating());
+	
+			// Executes the statement and checks for failure
+			if (prep.executeUpdate() == 0) {
+				throw new SQLException(
+						"Insertion into vandyupon.dining failed for an unknown reason");
+			}
+			return 1;
 		}
-		return 1;
-		
+		// else The Location or Rating is not valid
+			return -1;
 	}
 	
+	// This method updates the rating of a Location from a Device that has already sent a rating
 	private int updateRating(DiningRating dp) throws SQLException {
-		String sql;
-		Connection conn = ds_.getConnection();
-		
-		sql = "UPDATE DiningRatings SET rating= ? WHERE DeviceID= ? AND loc = ?";
-		
-		PreparedStatement prep = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-		prep.setInt(1, dp.getRating());
-		prep.setString(2, dp.getDeviceID());
-		prep.setInt(3, dp.getLocation());
-
-		
-		if (prep.executeUpdate() == 0) {
-			throw new SQLException(
-					"Insertion into vandyupon.dining failed for an unknown reason");
+		if((checkLocation(dp.getLocation()))&&(checkRating(dp.getRating()))){
+			String sql;
+			Connection conn = ds_.getConnection();
+			
+			// Updates the rating
+			sql = "UPDATE DiningRatings SET rating= ? WHERE DeviceID= ? AND loc = ?";
+			
+			// This block puts the passed in values into the SQL statement
+			PreparedStatement prep = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			prep.setInt(1, dp.getRating());
+			prep.setString(2, dp.getDeviceID());
+			prep.setInt(3, dp.getLocation());
+	
+			// Executes the statement and checks for failure
+			if (prep.executeUpdate() == 0) {
+				throw new SQLException(
+						"Insertion into vandyupon.dining failed for an unknown reason");
+			}
+			return 1;
 		}
-		return 1;
+		// else The Location or Rating is not valid
+		return -1; 
 		
 	}
 	
 	// This method is used to see if the request is already in the Database
 	private boolean checkForEntry(DiningRating dp) throws SQLException {
-		
 		Connection conn = ds_.getConnection();
-		
 		String sql;
 		
+		// The statement tries to select from the DiningRatings table to see if an entry exists
 		sql = "SELECT * FROM DiningRatings WHERE deviceid LIKE ? AND loc LIKE ?";
 		
-		PreparedStatement prep = conn.prepareStatement(sql,
-				Statement.RETURN_GENERATED_KEYS);
-		
+		// This block places the passed values into the above statement
+		PreparedStatement prep = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 		prep.setString(1, dp.getDeviceID());
-		
 		prep.setInt(2, dp.getLocation());
 		
+		// Execute the statement
 		ResultSet rs = prep.executeQuery();
 		
+		// Tests to see if anything was returened from the SQL query
 		try{
 			if(rs.next())
 				return true;
@@ -107,17 +119,25 @@ public class DiningHandler implements NotificationHandler {
 	
 	// This method performs the SQL to get the rating for a particular location.
 	private int getRating(DiningRatingRequest drr) throws SQLException {
-		String sql;
 		
-		Connection conn = ds_.getConnection();
-		
-		sql = "SELECT AVG(rating) FROM DiningRatings WHERE loc=?";
-		PreparedStatement prep = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-		prep.setInt(1, drr.getLocation());
-		
-		ResultSet rs = prep.executeQuery();
-		rs.next();
-		return rs.getInt(1);
+		if(checkLocation(drr.getLocation())){
+			String sql;
+			Connection conn = ds_.getConnection();
+			
+			// The statement returns the average of the ratings for a particular location
+			sql = "SELECT AVG(rating) FROM DiningRatings WHERE loc=?";
+			
+			// The block places the passed values into the above statement
+			PreparedStatement prep = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			prep.setInt(1, drr.getLocation());
+			
+			// Executes and returns
+			ResultSet rs = prep.executeQuery();
+			rs.next();
+			return rs.getInt(1);
+		}
+		else// The Location or Rating is not valid
+			return -1;
 	}
 	
 	// This method is used to check what type of DiningRating we have.
@@ -130,6 +150,16 @@ public class DiningHandler implements NotificationHandler {
 			return 0;
 	}
 	
+	// The method validates the location.
+	private boolean checkLocation(int loc){
+		return ((loc>0)&&(loc<=47));
+	}
+	
+	// This method validates the rating.
+	private boolean checkRating(int rat){
+		return ((rat>0)&&(rat<=5));
+	}
+	
 	// This method actually handles the DiningRatings and DiningRatingRequests.
 	// We perform the proper functions based on the type we receive.  After that,
 	// we return the proper response type.  If we do not receive a type we know 
@@ -137,19 +167,24 @@ public class DiningHandler implements NotificationHandler {
 	public ResponseNotification handle(Notification n) {
 		
 		if(checkForType(n) == 1)/* The type is DiningRating */{
+			
+			// Cast the Generic Notification as DiningRating
 			DiningRating dr = (DiningRating) n;
 			
 			try{
 				if(!(checkForEntry(dr))){
+					
+					// Add the rating
 					addRating(dr);
 					return new DiningRatingResponse("dummy", null, true);
-					//Must put something for ResponseType.. the responseHandler will call it.
+					// Must put something for ResponseType.. the responseHandler will call it.
 				}
 				else
 				{
+					// Update the rating
 					updateRating(dr);
 					return new DiningRatingResponse("dummy", null, true);
-					//Must put something for ResponseType.. the responseHandler will call it.
+					// Must put something for ResponseType.. the responseHandler will call it.
 				}
 			}
 			catch(SQLException e){
@@ -158,9 +193,13 @@ public class DiningHandler implements NotificationHandler {
 			}
 		}
 		else if(checkForType(n) == 2)/* The type is DiningRatingRequest */{
+			
+			// Cast the generic notification as DiningRatingRequet
 			DiningRatingRequest drr = (DiningRatingRequest) n;
 			
 			try{
+				
+				// Get the average rating.
 				int avgRating = getRating(drr);
 				return new DiningRatingRequestResponse("dummy", null, avgRating, true);
 			}
