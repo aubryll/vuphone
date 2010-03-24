70ee68892b6e984eff679e4c1f87f2315992454d
sma1Chan <sma1Chan@963311a8-3e45-11de-917f-4137a9cc17b9>
2010-03-24 01:52:31 +0000

    Made some changes to shows pictures from the xml file.  Also added 3 sections and display information loaded from the XML file for each waypoint.

---
+++ trunk/projects/campustour-iphone/SitOnClassViewController.h
@@ -0,0 +1,16 @@
+//
+//  SitOnClassViewController.h
+//  Campus Tour
+//
+//  Created by sma1 on 3/23/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SitOnClassViewController : UIViewController <UITableViewDataSource, UITableViewDelegate> {
+
+}
+
+@end
