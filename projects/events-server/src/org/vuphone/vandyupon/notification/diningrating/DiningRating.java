450d93843d1e3cd7b2e218e37d6e9d533ad65541
michael.j.t.fagan <michael.j.t.fagan@963311a8-3e45-11de-917f-4137a9cc17b9>
2010-04-09 02:06:02 +0000

    Added Doxygen style comments to all my code

--- projects/events-server/src/org/vuphone/vandyupon/notification/diningrating/DiningRating.java
+++ projects/events-server/src/org/vuphone/vandyupon/notification/diningrating/DiningRating.java
@@ -1,10 +1,28 @@
-// Copyright 2010 Michael Fagan
+/**
+ * @author Michael Fagan                                            
+ *                      
+ * @section LICENCE
+ * Licensed under the Apache License, Version 2.0 (the "License");         
+ * you may not use this file except in compliance with the License.        
+ * You may obtain a copy of the License at                                 
+ *                                                                         
+ * http://www.apache.org/licenses/LICENSE-2.0                              
+ *                                                                         
+ * Unless required by applicable law or agreed to in writing, software     
+ * distributed under the License is distributed on an "AS IS" BASIS,       
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and     
+ * limitations under the License. 
+ * 
+ * @section DESCRIPTION
+ * 
+ * This class will be used to hold a Notification as it pertains to a Dining Rating.
+ */
 
 package org.vuphone.vandyupon.notification.diningrating;
 
 import org.vuphone.vandyupon.notification.Notification;
 
-// This class will be used to hold a Notification as it pertains to a Dining Rating.
 public class DiningRating extends Notification{
 
 	// This variable is for the integer mapped to the proper location
@@ -14,13 +32,20 @@ public class DiningRating extends Notification{
 	// This variable is used to hold the ID of the device
 	private String deviceID_;
 	
-	// Default Ctor
+	/**
+	 * The default Ctor
+	 */
 	public DiningRating(){
 		super("diningrating");
 	}
 	
-	// Ctor for a Rating Call
-	// Here we will take in a location, a rating, and a device ID.
+	/**
+	 * Ctor for a Rating Addition
+	 *  
+	 * @param location The int representing the location being looked up
+	 * @param rating The rating being added
+	 * @param deviceID The ID of the device making the request
+	 */
 	public DiningRating(int location, int rating, String deviceID){
 		super("diningrating");
 		loc_ = location;
@@ -28,32 +53,56 @@ public class DiningRating extends Notification{
 		deviceID_ = deviceID;
 	}
 	
-	// Gets the location
+	/**
+	 * Returns the location in the container
+	 *  
+	 * @return The location being requested
+	 */
 	public int getLocation(){
 		return loc_;
 	}
 	
-	// Gets the rating
+	/**
+	 * Returns the rating in the container
+	 *  
+	 * @return The rating being added
+	 */
 	public int getRating(){
 		return rating_;
 	}
 	
-	// Gets the Device ID
+	/**
+	 * Returns the deviceID in the container
+	 *  
+	 * @return The deviceID that made the request
+	 */
 	public String getDeviceID(){
 		return deviceID_;
 	}
 	
-	// Sets the location
+	/**
+	 * Sets the location being requested
+	 *  
+	 * @param location The location to be set
+	 */
 	public void setLocation(int location){
 		loc_ = location;
 	}
 	
-	// Sets the rating
+	/**
+	 * Sets the location being requested
+	 *  
+	 * @param rating The rating to be added
+	 */
 	public void setRating(int rating){
 		rating_ = rating;
 	}
 	
-	// Sets the Device ID
+	/**
+	 * Sets the deviceID making the request
+	 *  
+	 * @param deviceID The ID of the device making the request
+	 */
 	public void setDeviceID(String deviceID){
 		deviceID_ = deviceID;
 	}
--- projects/events-server/src/org/vuphone/vandyupon/notification/diningrating/DiningRatingRequest.java
+++ projects/events-server/src/org/vuphone/vandyupon/notification/diningrating/DiningRatingRequest.java
@@ -1,11 +1,31 @@
-// Copyright 2010 Michael Fagan
+/**
+ * @author Michael Fagan                                            
+ *                      
+ * @section LICENCE
+ * Licensed under the Apache License, Version 2.0 (the "License");         
+ * you may not use this file except in compliance with the License.        
+ * You may obtain a copy of the License at                                 
+ *                                                                         
+ * http://www.apache.org/licenses/LICENSE-2.0                              
+ *                                                                         
+ * Unless required by applicable law or agreed to in writing, software     
+ * distributed under the License is distributed on an "AS IS" BASIS,       
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and     
+ * limitations under the License. 
+ * 
+ * @section DESCRIPTION
+ * 
+ * This class will be used to hold a Notification as it pertains to a Dining Rating Request.
+ * It will be used to handle a request for a Rating at a particular location
+ */
 
 package org.vuphone.vandyupon.notification.diningrating;
 
+import java.sql.SQLException;
+
 import org.vuphone.vandyupon.notification.Notification;
 
-// This class will be used to hold a Notification as it pertains to a Dining Rating Request.
-// It will be used to handle a request for a Rating at a particular location
 public class DiningRatingRequest extends Notification{
 
 	// This variable is for the integer mapped to the proper location
@@ -13,35 +33,57 @@ public class DiningRatingRequest extends Notification{
 	// This variable is used to hold the ID of the device
 	private String deviceID_;
 	
-	// Default Ctor
+	/**
+	 * The default Ctor
+	 */
 	public DiningRatingRequest(){
 		super("diningratingrequest");
 	}
 	
-	// Ctor for a Rating Request
-	// Here we will take in only a location
+	/**
+	 * Ctor for a Rating Request
+	 *  
+	 * @param location The int representing the location being looked up
+	 * @param deviceID The ID of the device making the request
+	 */
 	public DiningRatingRequest(int location, String deviceID){
 		super("diningratingrequest");
 		loc_ = location;
 		deviceID_ = deviceID;
 	}
 	
-	// Gets the location
+	/**
+	 * Returns the location in the container
+	 *  
+	 * @return The location being requested
+	 */
 	public int getLocation(){
 		return loc_;
 	}
 	
-	// Gets the Device ID
+	/**
+	 * Returns the deviceID in the container
+	 *  
+	 * @return The deviceID that made the request
+	 */
 	public String getDeviceID(){
 		return deviceID_;
 	}
 	
-	// Sets the location
+	/**
+	 * Sets the location being requested
+	 *  
+	 * @param location The location to be set
+	 */
 	public void setLocation(int location){
 		loc_ = location;
 	}
 	
-	// Sets the Device ID
+	/**
+	 * Sets the deviceID making the request
+	 *  
+	 * @param deviceID The ID of the device making the request
+	 */
 	public void setDeviceID(String deviceID){
 		deviceID_ = deviceID;
 	}
