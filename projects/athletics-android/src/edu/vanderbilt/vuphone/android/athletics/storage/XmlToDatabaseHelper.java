8c3e970c4ca37fb2dd90ac8c2861287ec46cf123
zachary.tyler.mccormick <zachary.tyler.mccormick@963311a8-3e45-11de-917f-4137a9cc17b9>
2010-04-15 01:02:31 +0000

    Fixed dataVersion updating system.  Cleaned up methods to add items to the database.  Cleaned up excessive logging.

--- projects/athletics-android/src/edu/vanderbilt/vuphone/android/athletics/storage/XmlToDatabaseHelper.java
+++ projects/athletics-android/src/edu/vanderbilt/vuphone/android/athletics/storage/XmlToDatabaseHelper.java
@@ -1,11 +1,9 @@
 package edu.vanderbilt.vuphone.android.athletics.storage;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -19,9 +17,7 @@ import org.xml.sax.SAXException;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.database.Cursor;
 import android.database.SQLException;
-import edu.vanderbilt.vuphone.android.athletics.R;
 
 /**
  * Multifunction XML and Database reading/building class. On construction, the
@@ -33,7 +29,7 @@ import edu.vanderbilt.vuphone.android.athletics.R;
  * 
  */
 public class XmlToDatabaseHelper {
-	private static String versionFilename = "data.version";
+	private static String dataVersion = "data.version";
 	private static String versionURL = "http://people.vanderbilt.edu/~zach.mccormick/data.version";
 	private static String dataURL = "http://people.vanderbilt.edu/~zach.mccormick/data.xml";
 	private static String appVersion = "0.1";
@@ -63,19 +59,17 @@ public class XmlToDatabaseHelper {
 		 * of check is via an Android preference.
 		 */
 		System.out.println("Checking for existence of version preference...");
-		String version_file = this.myContext.getString(R.string.VERSION_FILE);
-		String app_version = appVersion;
 
 		SharedPreferences settings = this.myContext.getSharedPreferences(
-				"athletics_database", this.myContext.MODE_PRIVATE);
-		if (!settings.contains(version_file)) {
-		    System.out.println("Version preference does not exist.");
+				"athletics_database", Context.MODE_PRIVATE);
+		if (!settings.contains(dataVersion)) {
+			System.out.println("Version preference does not exist.");
 
-		    SharedPreferences.Editor prefs = settings.edit();
-		    prefs.putString(version_file, app_version);
-		    prefs.commit();
+			SharedPreferences.Editor prefs = settings.edit();
+			prefs.putString(dataVersion, "0");
+			prefs.commit();
 		} else {
-			System.out.println("Version file exists.");
+			System.out.println("Version preference exists.");
 		}
 	}
 
@@ -112,27 +106,40 @@ public class XmlToDatabaseHelper {
 	public boolean isUpdated(Context myContext) {
 		System.out.println("Checking if database is updated...");
 		this.myContext = myContext;
+		BufferedReader onlineBufferedReader;
 		try {
-			FileInputStream fileReader = myContext.openFileInput(myContext
-					.getString(R.string.VERSION_FILE));
-			BufferedReader onlineBufferedReader = new BufferedReader(
-					new InputStreamReader(new URL(myContext
-							.getString(R.string.VERSION_URL)).openStream()), 4);
-			BufferedReader offlineBufferedReader = new BufferedReader(
-					new InputStreamReader(fileReader), 4);
-			String onlineVersion = onlineBufferedReader.readLine();
-			String offlineVersion = offlineBufferedReader.readLine();
-			if (onlineVersion.equalsIgnoreCase(offlineVersion) == false) {
-				System.out.println("Database is not up to date.");
-				return false;
-			} else {
-				System.out.println("Database is up to date.");
-				return true;
-			}
-		} catch (Exception e) {
+			onlineBufferedReader = new BufferedReader(new InputStreamReader(
+					new URL(versionURL).openStream()));
+		} catch (MalformedURLException e1) {
+			e1.printStackTrace();
+			System.out
+					.println("URL error occurred when reading online version file.");
+			return false;
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			System.out
+					.println("IO error occurred when reading online version file.");
+			return false;
+		}
+		SharedPreferences settings = this.myContext.getSharedPreferences(
+				"athletics_database", Context.MODE_PRIVATE);
+		String onlineVersion;
+		try {
+			onlineVersion = onlineBufferedReader.readLine();
+		} catch (IOException e) {
 			e.printStackTrace();
-			// TODO: make this function throw something else to signal an error
+			System.out
+					.println("IO error occurred when reading online version file.");
+			return false;
+		}
+		String offlineVersion = settings.getString(dataVersion, "0");
+		if (onlineVersion.equalsIgnoreCase(offlineVersion)) {
+			System.out.println("Database is up to date.");
 			return true;
+		} else {
+			System.out
+					.println("Database is not up to date or an error occurred.");
+			return false;
 		}
 	}
 
@@ -143,27 +150,44 @@ public class XmlToDatabaseHelper {
 	 */
 	private boolean updateVersion() {
 		System.out.println("Attempting to update version file...");
+		SharedPreferences settings = this.myContext.getSharedPreferences(
+				"athletics_database", Context.MODE_PRIVATE);
+		SharedPreferences.Editor prefs = settings.edit();
+
+		BufferedReader onlineVersionBufferedReader;
 		try {
-			FileOutputStream versionOutput = myContext.openFileOutput(myContext
-					.getString(R.string.VERSION_FILE), 0);
-			PrintStream versionPrinter = new PrintStream(versionOutput);
-			BufferedReader onlineVersionBufferedReader = new BufferedReader(
-					new InputStreamReader(new URL(myContext
-							.getString(R.string.VERSION_URL)).openStream()), 4);
-			versionPrinter.println(onlineVersionBufferedReader);
-			versionPrinter.close();
-			System.out.println("Version file updated.");
-			return true;
-		} catch (Exception e) {
-			e.printStackTrace();
+			onlineVersionBufferedReader = new BufferedReader(
+					new InputStreamReader(new URL(versionURL).openStream()));
+		} catch (MalformedURLException e1) {
+			e1.printStackTrace();
+			System.out
+					.println("URL error occurred when reading online version file.");
+			return false;
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			System.out
+					.println("IO error occurred when reading online version file.");
 			return false;
 		}
+		try {
+			prefs
+					.putString(dataVersion, onlineVersionBufferedReader
+							.readLine());
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			System.out
+					.println("IO error occurred when reading online version file.");
+			return false;
+		}
+		prefs.commit();
+		System.out.println("Version updated.");
+		return true;
 	}
 
 	/**
 	 * Parses the XML file online into a local dom object.
 	 */
-	private void parseXmlFile() {
+	private boolean parseXmlFile() {
 		System.out.println("Parsing XML file to DOM object...");
 		// create a DocumentBuilderFactory
 		// documentation: "defines a factory API that enables applications to
@@ -175,18 +199,37 @@ public class XmlToDatabaseHelper {
 		// results of the parser
 		try {
 			DocumentBuilder db = dbf.newDocumentBuilder();
-			dom = db.parse(new URL
-					(myContext.getString
-							(R.string.DATA_URL))
-					.openStream());
+			dom = db.parse(new URL(dataURL).openStream());
 			System.out.println("XML file parsed to DOM object.");
 		} catch (ParserConfigurationException pce) {
-			pce.printStackTrace(); // catches parser configuration exceptions
+			pce.printStackTrace();
+			System.out.println("Parser Configuration error");
+			return false;
 		} catch (SAXException se) {
-			se.printStackTrace(); // catches SAX parser exceptions (bad XML)
+			se.printStackTrace();
+			System.out.println("XML error");
+			return false;
 		} catch (IOException ioe) {
-			ioe.printStackTrace(); // catches IO exceptions
+			ioe.printStackTrace();
+			System.out.println("IO error");
+			return false;
 		}
+		return true;
+	}
+
+	public boolean addItems(Element rootElement, String tag) {
+		NodeList itemNodeList = rootElement.getElementsByTagName(tag);
+		// iterate through the nodelist and add each item to the database
+		if (itemNodeList != null && itemNodeList.getLength() > 0) {
+			for (int i = 0; i < itemNodeList.getLength(); i++) {
+				// get the i(th) item in the list
+				Element el = (Element) itemNodeList.item(i);
+				addItemToDatabase(el, tag);
+			}
+		} else {
+			return false;
+		}
+		return true;
 	}
 
 	/**
@@ -200,92 +243,12 @@ public class XmlToDatabaseHelper {
 		// get the root element, which will be the <data> tag
 		Element rootElement = dom.getDocumentElement();
 
-		System.out.println("Adding news items...");
-		// get a nodelist of news items
-		NodeList newsNodeList = rootElement.getElementsByTagName("news");
-
-		// iterate through the nodelist and add each news to the database
-		if (newsNodeList != null && newsNodeList.getLength() > 0) {
-			for (int i = 0; i < newsNodeList.getLength(); i++) {
-				// get the i(th) news item in the list
-				Element el = (Element) newsNodeList.item(i);
-				addNewsItemToDatabase(el);
-			}
-		}
-		System.out.println("News items added.");
-		System.out.println("Adding games...");
-
-		// get a nodelist of games
-		NodeList gameNodeList = rootElement.getElementsByTagName("game");
-
-		// iterate through the nodelist and add each game to the database
-		if (gameNodeList != null && gameNodeList.getLength() > 0) {
-			for (int i = 0; i < gameNodeList.getLength(); i++) {
-				// get the i(th) game in the list
-				Element el = (Element) gameNodeList.item(i);
-				addGameToDatabase(el);
-			}
-		}
-		System.out.println("Games added.");
-
-		System.out.println("Adding sports...");
-
-		// get a nodelist of games
-		NodeList sportNodeList = rootElement.getElementsByTagName("sport");
-
-		// iterate through the nodelist and add each game to the database
-		if (sportNodeList != null && sportNodeList.getLength() > 0) {
-			for (int i = 0; i < sportNodeList.getLength(); i++) {
-				// get the i(th) game in the list
-				Element el = (Element) sportNodeList.item(i);
-				addSportToDatabase(el);
-			}
-		}
-		System.out.println("Sports added.");
-
-		System.out.println("Adding conferences...");
-
-		// get a nodelist of conferences
-		NodeList conferenceNodeList = rootElement
-				.getElementsByTagName("conference");
-
-		// iterate through the nodelist and add each conference to the database
-		if (conferenceNodeList != null && conferenceNodeList.getLength() > 0) {
-			for (int i = 0; i < conferenceNodeList.getLength(); i++) {
-				// get the i(th) game in the list
-				Element el = (Element) conferenceNodeList.item(i);
-				addConferenceToDatabase(el);
-			}
-		}
-		System.out.println("Conferences added.");
-		System.out.println("Adding teams...");
-
-		// get a nodelist of teams
-		NodeList teamNodeList = rootElement.getElementsByTagName("team");
-
-		// iterate through the nodelist and add each team to the database
-		if (teamNodeList != null && teamNodeList.getLength() > 0) {
-			for (int i = 0; i < teamNodeList.getLength(); i++) {
-				// get the i(th) team in the list
-				Element el = (Element) teamNodeList.item(i);
-				addTeamToDatabase(el);
-			}
-		}
-		System.out.println("Teams added.");
-		System.out.println("Adding players...");
-
-		// get a nodelist of players
-		NodeList playerNodeList = rootElement.getElementsByTagName("player");
-
-		// iterate through the nodelist and add each player to the database
-		if (playerNodeList != null && playerNodeList.getLength() > 0) {
-			for (int i = 0; i < playerNodeList.getLength(); i++) {
-				// get the i(th) player in the list
-				Element el = (Element) playerNodeList.item(i);
-				addPlayerToDatabase(el);
-			}
-		}
-		System.out.println("Players added.");
+		addItems(rootElement, "news");
+		addItems(rootElement, "game");
+		addItems(rootElement, "sport");
+		addItems(rootElement, "conference");
+		addItems(rootElement, "team");
+		addItems(rootElement, "player");
 
 		closeDatabase();
 		updateVersion();
@@ -297,112 +260,59 @@ public class XmlToDatabaseHelper {
 	 * @param el
 	 *            element from nodelist of games
 	 */
-	private void addGameToDatabase(Element el) {
-		// for each game, we will get the following attributes
-		int id = Integer.parseInt(el.getAttribute("game_id"));
-		int hometeam = Integer.parseInt(el.getAttribute("hometeam"));
-		int awayteam = Integer.parseInt(el.getAttribute("awayteam"));
-		int sport = Integer.parseInt(el.getAttribute("sport"));
-		String time = el.getAttribute("time");
-		int homescore = Integer.parseInt(el.getAttribute("homescore"));
-		int awayscore = Integer.parseInt(el.getAttribute("awayscore"));
-		System.out.println("Adding game to database: " + awayteam + " @ "
-				+ hometeam + "...");
-		myDatabaseHelper.createGame(id, hometeam, awayteam, sport, time,
-				homescore, awayscore);
-		System.out.println("Added game to database.");
-	}
-
-	/**
-	 * this function will add each team to the database
-	 * 
-	 * @param el
-	 *            element from nodelist of teams
-	 */
-	private void addTeamToDatabase(Element el) {
-		// for each team, get the following attributes
-		int id = Integer.parseInt(el.getAttribute("team_id"));
-		String school = el.getAttribute("school");
-		String name = el.getAttribute("name");
-		int conference = Integer.parseInt(el.getAttribute("conference"));
-
-		System.out.println("Adding team to database: " + school + "...");
-
-		myDatabaseHelper.createTeam(id, school, name, conference);
-		System.out.println("Added team to database.");
-	}
-
-	/**
-	 * this function will add each player to the database
-	 * 
-	 * @param team
-	 *            the team the player comes from
-	 * @param el
-	 *            element from nodelist of players
-	 */
-	private void addPlayerToDatabase(Element el) {
-		int id = Integer.parseInt(el.getAttribute("player_id"));
-		String firstname = el.getAttribute("firstname");
-		String lastname = el.getAttribute("lastname");
-		int number = Integer.parseInt(el.getAttribute("number"));
-		String position = el.getAttribute("position");
-		int sport = Integer.parseInt(el.getAttribute("sport"));
-		int team = Integer.parseInt(el.getAttribute("team"));
-
-		System.out.println("Adding player to database: " + firstname + " "
-				+ lastname + "...");
-
-		myDatabaseHelper.createPlayer(id, firstname, lastname, number,
-				position, sport, team);
-		System.out.println("Added player to database.");
-	}
-
-	/**
-	 * this function will add each news item to the database
-	 * 
-	 * @param el
-	 *            element from nodelist of news items
-	 */
-	private void addNewsItemToDatabase(Element el) {
-		// for each news item, we will get the following attributes
-		int id = Integer.parseInt(el.getAttribute("news_id"));
-		String title = el.getAttribute("title");
-		String body = el.getAttribute("body");
-		String link = el.getAttribute("link");
-		String date = el.getAttribute("date");
-		int sport = Integer.parseInt(el.getAttribute("sport"));
-
-		System.out.println("Adding news item to database: "
-				+ title.substring(0, 15) + "...");
-
-		myDatabaseHelper.createNewsItem(id, title, body, link, date, sport);
-		System.out.println("Added news item to database.");
-
-	}
-
-	private void addConferenceToDatabase(Element el) {
-		// for each conference, we will get the following attributes
-		int id = Integer.parseInt(el.getAttribute("conference_id"));
-		String name = el.getAttribute("name");
-		String abbreviation = el.getAttribute("abbreviation");
-
-		System.out.println("Adding conference to database: " + abbreviation
-				+ "...");
-
-		myDatabaseHelper.createConference(id, name, abbreviation);
-		System.out.println("Added conference to database.");
-
-	}
-
-	private void addSportToDatabase(Element el) {
-		// for each team, we will get the following attributes
-		int id = Integer.parseInt(el.getAttribute("sport_id"));
-		String name = el.getAttribute("name");
-
-		System.out.println("Adding sport to database: " + name + "...");
-
-		myDatabaseHelper.createSport(id, name);
-		System.out.println("Added sport to database.");
-
+	private boolean addItemToDatabase(Element el, String tag) {
+		long row = -1;
+		if (tag.equalsIgnoreCase("news")) {
+			int id = Integer.parseInt(el.getAttribute("news_id"));
+			String title = el.getAttribute("title");
+			String body = el.getAttribute("body");
+			String link = el.getAttribute("link");
+			String date = el.getAttribute("date");
+			int sport = Integer.parseInt(el.getAttribute("sport"));
+			row = myDatabaseHelper.createNewsItem(id, title, body, link, date,
+					sport);
+		} else if (tag.equalsIgnoreCase("game")) {
+			int id = Integer.parseInt(el.getAttribute("game_id"));
+			int hometeam = Integer.parseInt(el.getAttribute("hometeam"));
+			int awayteam = Integer.parseInt(el.getAttribute("awayteam"));
+			int sport = Integer.parseInt(el.getAttribute("sport"));
+			String time = el.getAttribute("time");
+			int homescore = Integer.parseInt(el.getAttribute("homescore"));
+			int awayscore = Integer.parseInt(el.getAttribute("awayscore"));
+			row = myDatabaseHelper.createGame(id, hometeam, awayteam, sport,
+					time, homescore, awayscore);
+		} else if (tag.equalsIgnoreCase("sport")) {
+			int id = Integer.parseInt(el.getAttribute("sport_id"));
+			String name = el.getAttribute("name");
+			row = myDatabaseHelper.createSport(id, name);
+		} else if (tag.equalsIgnoreCase("conference")) {
+			int id = Integer.parseInt(el.getAttribute("conference_id"));
+			String name = el.getAttribute("name");
+			String abbreviation = el.getAttribute("abbreviation");
+			row = myDatabaseHelper.createConference(id, name, abbreviation);
+		} else if (tag.equalsIgnoreCase("team")) {
+			int id = Integer.parseInt(el.getAttribute("team_id"));
+			String school = el.getAttribute("school");
+			String name = el.getAttribute("name");
+			int conference = Integer.parseInt(el.getAttribute("conference"));
+			row = myDatabaseHelper.createTeam(id, school, name, conference);
+		} else if (tag.equalsIgnoreCase("player")) {
+			int id = Integer.parseInt(el.getAttribute("player_id"));
+			String firstname = el.getAttribute("firstname");
+			String lastname = el.getAttribute("lastname");
+			int number = Integer.parseInt(el.getAttribute("number"));
+			String position = el.getAttribute("position");
+			int sport = Integer.parseInt(el.getAttribute("sport"));
+			int team = Integer.parseInt(el.getAttribute("team"));
+			row = myDatabaseHelper.createPlayer(id, firstname, lastname,
+					number, position, sport, team);
+		}
+		if (row == -1) {
+			System.out.println("Error occurred on inserting a " + tag);
+			return false;
+		} else {
+			System.out.println(tag + "added with row id " + row);
+			return true;
+		}
 	}
 }
